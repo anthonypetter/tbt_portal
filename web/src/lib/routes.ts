ec15d4cbc61d043088fbb9bcf6c4ec9c174efbe8
anthonypetter <anthonypsalazar9@gmail.com>
2022-04-25 21:52:00 -0600

    add util to keep track of routes

---
+++ web/src/lib/routes.ts
@@ -0,0 +1,38 @@
+export const Routes = {
+  home: {
+    href: () => "/",
+    path: () => "/",
+  },
+  liveView: {
+    href: () => "/live-view",
+    path: () => "/live-view",
+  },
+  mySchedule: {
+    href: () => "/my-schedule",
+    path: () => "/my-schedule",
+  },
+  organizations: {
+    href: () => "/organizations",
+    path: () => "/organizations",
+  },
+  engagements: {
+    href: () => "/engagements",
+    path: () => "/engagements",
+  },
+  classrooms: {
+    href: () => "/classrooms",
+    path: () => "/classrooms",
+  },
+  users: {
+    href: () => "/users",
+    path: () => "/users",
+  },
+  teachers: {
+    href: () => "/teachers",
+    path: () => "/teachers",
+  },
+  recordings: {
+    href: () => "/recordings",
+    path: () => "/recordings",
+  },
+};
