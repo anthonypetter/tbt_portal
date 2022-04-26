593d7a823cfae5758e5a25209702f0a350803684
anthonypetter <anthonypsalazar9@gmail.com>
2022-04-26 16:57:29 -0600

    add signout

--- web/src/components/AppLayout.tsx
+++ web/src/components/AppLayout.tsx
@@ -9,7 +9,7 @@ import {
 } from "@heroicons/react/outline";
 import { SearchIcon } from "@heroicons/react/solid";
 import clsx from "clsx";
-import { Routes } from "@lib/routes";
+import { Routes } from "@utils/routes";
 import { FaRegBuilding, FaGraduationCap } from "react-icons/fa";
 import { SiGoogleclassroom } from "react-icons/si";
 import { MdWorkspacesOutline } from "react-icons/md";
@@ -18,6 +18,8 @@ import { BiVideoRecording } from "react-icons/bi";
 import { RiSignalTowerFill } from "react-icons/ri";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useAuth } from "./auth/AuthProvider";
+import Image from "next/image";
 
 function getNavigation(currentPathname: string) {
   return [
@@ -81,7 +83,6 @@ function getNavigation(currentPathname: string) {
 const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
 ];
 
 type Props = {
@@ -188,6 +189,7 @@ export function AppLayout({ children }: Props) {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-col flex-grow pt-5 bg-gray-700 overflow-y-auto">
             <div className="flex items-center flex-shrink-0 px-4">
+              {/* TODO: Replace */}
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
@@ -257,46 +259,7 @@ export function AppLayout({ children }: Props) {
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {userNavigation.map((item) => (
-                        <Menu.Item key={item.name}>
-                          {({ active }) => (
-                            <a
-                              href={item.href}
-                              className={clsx(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                <AvatarDropdown />
               </div>
             </div>
           </div>
@@ -311,5 +274,65 @@ export function AppLayout({ children }: Props) {
         </div>
       </div>
     </>
+  );
+}
+
+function AvatarDropdown() {
+  const auth = useAuth();
+
+  return (
+    <Menu as="div" className="ml-3 relative">
+      <div>
+        <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          <span className="sr-only">Open user menu</span>
+          <img
+            className="h-8 w-8 rounded-full"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {userNavigation.map((item) => (
+            <Menu.Item key={item.name}>
+              {({ active }) => (
+                <a
+                  href={item.href}
+                  className={clsx(
+                    active ? "bg-gray-100" : "",
+                    "block px-4 py-2 text-sm text-gray-700"
+                  )}
+                >
+                  {item.name}
+                </a>
+              )}
+            </Menu.Item>
+          ))}
+
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                onClick={() => auth.signOut()}
+                className={clsx(
+                  active ? "bg-gray-100" : "",
+                  "block px-4 py-2 text-gray-700 text-sm cursor-pointer"
+                )}
+              >
+                Sign out
+              </a>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
