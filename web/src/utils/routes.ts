bc5d053f1e3ec95481c7bcc154fc7d2ffc12d2f4
scottkuecker <kueckerscott@gmail.com>
2022-04-26 16:58:26 -0600

    add routes helper

---
+++ web/src/utils/routes.ts
@@ -0,0 +1,42 @@
+export const Routes = {
+  home: {
+    href: () => "/",
+    path: () => "/",
+  },
+  login: {
+    href: () => "/login",
+    path: () => "/login",
+  },
+  liveView: {
+    href: () => "/live-view",
+    path: () => "/live-view",
+  },
+  mySchedule: {
+    href: () => "/my-schedule",
+    path: () => "/my-schedule",
+  },
+  organizations: {
+    href: () => "/organizations",
+    path: () => "/organizations",
+  },
+  engagements: {
+    href: () => "/engagements",
+    path: () => "/engagements",
+  },
+  classrooms: {
+    href: () => "/classrooms",
+    path: () => "/classrooms",
+  },
+  users: {
+    href: () => "/users",
+    path: () => "/users",
+  },
+  teachers: {
+    href: () => "/teachers",
+    path: () => "/teachers",
+  },
+  recordings: {
+    href: () => "/recordings",
+    path: () => "/recordings",
+  },
+};
