a15c3f5cf4cd1c90afa272ffc9c357bf6d86189d
scottkuecker <kueckerscott@gmail.com>
2022-04-27 15:05:07 -0600

    add user service

---
+++ graphql/src/services/user/index.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@lib/prisma-client";
+import { User } from "@prisma/client";
+
+export const UserService = {
+  async getUserByCognitoSub(cognitoSub: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        cognitoSub,
+      },
+    });
+
+    return user;
+  },
+};
