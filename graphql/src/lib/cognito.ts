e6040fdb448a85f3eaef3131fecef5aef000d888
scottkuecker <kueckerscott@gmail.com>
2022-04-27 15:03:49 -0600

    use access token in auth header to get cognito user

---
+++ graphql/src/lib/cognito.ts
@@ -0,0 +1,53 @@
+import {
+  CognitoIdentityProvider,
+  GetUserCommandOutput,
+} from "@aws-sdk/client-cognito-identity-provider";
+import { CognitoRegion } from "../config";
+import { AuthenticationError } from "apollo-server";
+import { fromJust } from "@utils/types";
+import { UserService } from "@services/user";
+import find from "lodash/find";
+
+const cognitoClient = new CognitoIdentityProvider({
+  region: CognitoRegion,
+});
+
+/**
+ * Gets a user based on cognito bearer token in auth header
+ * @param authHeader request authorization header
+ */
+
+export async function getUser(authHeader: string | undefined) {
+  if (!authHeader) {
+    throw new AuthenticationError("Auth header is missing.");
+  }
+
+  // Strip "Bearer"
+  const accessToken = authHeader.split(" ")[1];
+
+  // Get cognito user and inspect attributes
+  const cognitoUser = await getCognitoUserFromToken(accessToken);
+  const userAttributes = fromJust(
+    cognitoUser.UserAttributes,
+    "cognitoUser.UserAttributes"
+  );
+
+  const mCognitoSub = fromJust(
+    find(userAttributes, (attr) => attr.Name === "sub"),
+    "cognitoSub attribute"
+  ).Value;
+
+  const user = await UserService.getUserByCognitoSub(
+    fromJust(mCognitoSub, "cognitoSub value")
+  );
+
+  return user;
+}
+
+async function getCognitoUserFromToken(
+  token: string
+): Promise<GetUserCommandOutput> {
+  return cognitoClient.getUser({
+    AccessToken: token,
+  });
+}
--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -1,10 +1,13 @@
-import { ApolloServer, gql } from "apollo-server";
-import { PrismaClient } from "@prisma/client";
-import { context } from "./context";
+import { ApolloServer } from "apollo-server";
 import { resolvers, typeDefs } from "./schema";
+import { getContext } from "./context";
 
-const server = new ApolloServer({ typeDefs, resolvers, context });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: getContext,
+});
 
 server.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at: http://localhost:4000`)
+  console.log(`🚀 Server ready at: http://localhost:4000!`)
 );
