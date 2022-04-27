5dd3874fd47aa38e8f63b775480732aa0926f63b
anthonypetter <anthonypsalazar9@gmail.com>
2022-04-26 18:07:05 -0600

    update cognito stack

--- cdk-infra/bin/cdk-infra-stack.ts
+++ cdk-infra/bin/cdk-infra-stack.ts
@@ -1,15 +1,29 @@
 import { App, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { CognitoStack } from "../lib/cognito-stack";
+import { GraphQLServerStack } from "../lib/graphql-stack";
 
 export class PortalApp extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const cognitoPool = new CognitoStack(app, "PortalCognito", {
-      // env: context.env,
-      // url: `my.${context.domain}`,
-      // domain: context.domain,
+    const devEnv: StackProps["env"] = {
+      account: "014491063547",
+      region: "us-east-2",
+    };
+
+    const stageEnv: StackProps["env"] = {
+      account: "014491063547",
+      region: "us-east-2",
+    };
+
+    const cognito = new CognitoStack(app, "PortalCognito", {
+      env: devEnv,
+    });
+
+    new GraphQLServerStack(app, "GraphQlServerStack", {
+      env: devEnv,
+      userPool: cognito.userPool,
     });
   }
 }
