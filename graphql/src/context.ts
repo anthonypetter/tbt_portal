8b7ecbbaea699da4fb4c33e57dfdf8fcce3c8ca5
anthonypetter <anthonypsalazar9@gmail.com>
2022-04-27 15:03:49 -0600

    use access token in auth header to get cognito user

--- graphql/src/context.ts
+++ graphql/src/context.ts
@@ -1,11 +1,30 @@
-import { PrismaClient } from "@prisma/client";
+import { User } from "@prisma/client";
+import { UserService } from "@services/user";
+import { ExpressContext } from "apollo-server-express";
+import { getUser } from "@lib/cognito";
+import { AuthenticationError } from "apollo-server";
 
-const prisma = new PrismaClient();
-
-export interface Context {
-  prisma: PrismaClient;
-}
-
-export const context: Context = {
-  prisma: prisma,
+export type Context = {
+  authedUser: User;
+  UserService: typeof UserService;
 };
+
+export async function getContext({ req }: ExpressContext): Promise<Context> {
+  const authHeader = req.headers.authorization;
+  const authedUser = await getUser(authHeader);
+
+  if (!authedUser) {
+    throw new AuthenticationError("User not found.");
+  }
+
+  /**
+   * Intentionally leaving prisma off context. (for now)
+   * Idea is that all db access should go through a "service" layer.
+   */
+
+  return {
+    // prisma
+    authedUser,
+    UserService,
+  };
+}
