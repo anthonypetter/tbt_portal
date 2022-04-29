030055b73029ee71c99ee2a2ae08cfe0e19727a2
anthonypetter <anthonypsalazar9@gmail.com>
2022-04-29 15:49:07 -0600

    add favicon

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,16 +3,23 @@ import type { AppProps } from "next/app";
 import { AuthProvider } from "components/auth/AuthProvider";
 import { configureAmplify } from "@lib/amplify";
 import { Layout } from "components/Layout";
+import Head from "next/head";
 
 configureAmplify();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </>
   );
 }
 
