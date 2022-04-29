a56ff836ed26cf6891b8499bee873d2719639026
anthonypetter <anthonypsalazar9@gmail.com>
2022-04-29 12:54:08 -0600

    improve error component

--- web/src/components/ErrorBox.tsx
+++ web/src/components/ErrorBox.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { XCircleIcon } from "@heroicons/react/solid";
 
 type Props = {
-  errorMsg: string;
+  msg?: string;
+  subMsg?: string;
 };
 
-export function ErrorBox({ errorMsg }: Props) {
+export function ErrorBox({ msg, subMsg }: Props) {
+  const message = msg ?? "Looks like something went wrong.";
+
   return (
-    <div className="rounded-md bg-red-50 p-4">
+    <div className="mb-4 p-4 bg-red-50 border border-red-400 rounded-md">
       <div className="flex">
         <div className="flex-shrink-0">
-          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+          <XCircleIcon className="w-5 h-5 text-red-400" aria-hidden="true" />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-800">{errorMsg}</h3>
+          <h3 className="text-red-800 text-sm font-medium">{message}</h3>
+          {subMsg && <h3 className="mt-2 text-red-700 text-sm">{subMsg}</h3>}
         </div>
       </div>
     </div>
