18ff7aed63f45a763010f1b800ca4b06c10fe020
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-05 08:12:36 -0600

    remove hardcoded values

--- graphql/src/config/index.ts
+++ graphql/src/config/index.ts
@@ -1,4 +1,11 @@
 import { fromJust } from "../utils/types";
 
-export const COGNITO_USER_POOL_ID = fromJust(process.env.COGNITO_USER_POOL_ID);
-export const COGNITO_REGION = fromJust(process.env.COGNITO_REGION);
+export const COGNITO_USER_POOL_ID = fromJust(
+  process.env.COGNITO_USER_POOL_ID,
+  "process.env.COGNITO_USER_POOL_ID"
+);
+
+export const COGNITO_REGION = fromJust(
+  process.env.COGNITO_REGION,
+  "process.env.COGNITO_REGION"
+);
