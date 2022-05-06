8cb008834ebb974ec3d62e8245e07b779f203d06
scottkuecker <kueckerscott@gmail.com>
2022-05-06 11:47:58 -0600

    extract users table from users page

---
+++ web/src/components/users/UsersPage.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Button } from "../Button";
+import { InviteUserModal } from "../InviteUserModal";
+import { UsersPageQuery } from "@generated/graphql";
+import { gql } from "@apollo/client";
+import { triggerSuccessToast } from "../Toast";
+import { UsersTable } from "./UsersTable";
+
+type Props = {
+  users: NonNullable<UsersPageQuery["users"]>;
+  refetchUsers: () => void;
+};
+
+UsersPage.fragments = {
+  users: gql`
+    fragment Users on Query {
+      users {
+        email
+        role
+        accountStatus
+      }
+    }
+  `,
+};
+
+export function UsersPage({ users, refetchUsers }: Props) {
+  const [showInviteModal, setShowInviteModal] = useState(false);
+
+  return (
+    <>
+      <h1 className="text-2xl font-semibold text-gray-900 mb-6">Users</h1>
+
+      <div className="flex justify-end mb-6">
+        <Button onClick={() => setShowInviteModal(true)}>Invite User</Button>
+      </div>
+      <div className="mb-4 lg:mb-0">
+        <UsersTable users={users} />
+      </div>
+
+      <InviteUserModal
+        show={showInviteModal}
+        onCancel={() => setShowInviteModal(false)}
+        onSuccess={() => {
+          refetchUsers();
+          setShowInviteModal(false);
+          triggerSuccessToast({ message: "User successfully invited!" });
+        }}
+      />
+    </>
+  );
+}
---
+++ web/src/components/users/UsersTable.tsx
@@ -0,0 +1,66 @@
+import { useMemo } from "react";
+import { Column, Cell } from "react-table";
+import { Table } from "components/Table";
+import { UsersPageQuery } from "@generated/graphql";
+import { AccountStatusBadge } from "components/AccountStatusBadge";
+import { getTextForRole } from "components/RoleText";
+
+type Props = {
+  users: NonNullable<UsersPageQuery["users"]>;
+};
+
+export function UsersTable({ users }: Props) {
+  const { columns, data: tableData } = usePrepUserData(users);
+  return <Table columns={columns} data={tableData} />;
+}
+
+type UserTableData = {
+  email: string;
+  role: string;
+  accountStatus: string;
+};
+
+function usePrepUserData(users: NonNullable<UsersPageQuery["users"]>): {
+  data: UserTableData[];
+  columns: Column<UserTableData>[];
+} {
+  const columns: Column<UserTableData>[] = useMemo(() => {
+    return [
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+      },
+      {
+        Header: "Status",
+        accessor: "accountStatus",
+        Cell: ({ row }: Cell<UserTableData>) => {
+          return (
+            <AccountStatusBadge accountStatus={row.values.accountStatus} />
+          );
+        },
+      },
+    ];
+  }, []);
+
+  const emails = users.map((u) => u.email).join();
+
+  const data = useMemo(() => {
+    return users.map((user) => {
+      const roleText = getTextForRole(user.role);
+      return {
+        email: user.email,
+        role: roleText === "Administrator" ? "Admin" : roleText,
+        accountStatus: user.accountStatus,
+      };
+    });
+    // Used concatenated emails as a way to determine if users
+    // dependency has changed. (instead of stringifying an array of objects)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [emails]);
+
+  return { data, columns };
+}
