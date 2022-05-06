99d5b4e5be771e52bdac444ebaf26631c19513e3
scottkuecker <kueckerscott@gmail.com>
2022-05-06 11:15:32 -0600

    add context menu component

---
+++ web/src/components/ContextMenu.tsx
@@ -0,0 +1,56 @@
+import { DotsMenu } from "./DotsMenu";
+import { Menu } from "@headlessui/react";
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
+import clsx from "clsx";
+
+type Props = {
+  onClickEdit: () => void;
+  onClickDelete: () => void;
+};
+
+export function ContextMenu({ onClickEdit, onClickDelete }: Props) {
+  return (
+    <DotsMenu>
+      <Menu.Items className="absolute z-10 right-0 mt-2 w-56 bg-white rounded-md focus:outline-none shadow-lg origin-top-right ring-1 ring-black ring-opacity-5">
+        <div className="py-1">
+          <Menu.Item>
+            {({ active }) => (
+              <button
+                onClick={onClickEdit}
+                className={clsx(
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                  "group flex items-center px-4 py-2 w-full text-sm"
+                )}
+              >
+                <PencilAltIcon
+                  className="mr-3 w-5 h-5 text-gray-400 group-hover:text-gray-500"
+                  aria-hidden="true"
+                />
+                Edit
+              </button>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <button
+                onClick={onClickDelete}
+                className={clsx(
+                  active ? "bg-red-600 text-white" : "text-red-600",
+                  "group flex items-center px-4 py-2 w-full text-sm"
+                )}
+              >
+                <TrashIcon
+                  className={clsx(
+                    "mr-3 w-5 h-5 text-red-600 group-hover:text-white"
+                  )}
+                  aria-hidden="true"
+                />
+                Delete
+              </button>
+            )}
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+    </DotsMenu>
+  );
+}
