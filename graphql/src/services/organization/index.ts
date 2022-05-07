ec77bd2dc078bfb24091fd1b8dc54452d0d32f58
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-07 13:37:33 -0600

    add org crud operations to org service

--- graphql/src/services/organization/index.ts
+++ graphql/src/services/organization/index.ts
@@ -7,4 +7,49 @@ export const OrganizationService = {
     const organizations = await prisma.organization.findMany({ take });
     return organizations;
   },
+
+  async addOrg({
+    name,
+    district,
+    subDistrict,
+  }: {
+    name: string;
+    district?: string | null;
+    subDistrict?: string | null;
+  }): Promise<Organization> {
+    const organization = await prisma.organization.create({
+      data: {
+        name,
+        district: district === null ? undefined : district,
+        subDistrict: subDistrict === null ? undefined : subDistrict,
+      },
+    });
+    return organization;
+  },
+
+  // Prisma will ignore fields that are undefined and will not update them.
+  async editOrganization({
+    id,
+    name,
+    district,
+    subDistrict,
+  }: {
+    id: number;
+    name: string;
+    district?: string | null;
+    subDistrict?: string | null;
+  }): Promise<Organization> {
+    const organization = await prisma.organization.update({
+      where: { id },
+      data: { name, district, subDistrict },
+    });
+    return organization;
+  },
+
+  async deleteOrganization(id: number): Promise<Organization> {
+    const organization = await prisma.organization.delete({
+      where: { id },
+    });
+    return organization;
+  },
 };
