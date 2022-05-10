7da83e220c1ba34b31083c5aa373a18918410073
scottkuecker <kueckerscott@gmail.com>
2022-05-10 16:01:01 -0600

    update seeding with engagement and cohorts

--- graphql/prisma/seed.ts
+++ graphql/prisma/seed.ts
@@ -1,4 +1,5 @@
-import { Organization, PrismaClient, User } from "@prisma/client";
+import { Organization, Prisma, PrismaClient, User } from "@prisma/client";
+import { fromJust } from "../src/utils/types";
 
 const prisma = new PrismaClient();
 
@@ -10,15 +11,8 @@ async function main() {
     throw new Error("Unexpected null/undefiend value for APP_ENV");
   }
 
-  await prisma.user.createMany({
-    data: getUsers(env),
-  });
-  console.log("[🌱 Seed] - Users created.");
-
-  await prisma.organization.createMany({
-    data: getOrgs(),
-  });
-  console.log("[🌱 Seed] - Organizations created.");
+  const users = await createUsers(env);
+  await createOrgs(users);
 
   console.log("[🌱 Seed] - Finished seeding.");
 }
@@ -35,68 +29,178 @@ main()
 /**
  * Users
  */
-function getUsers(env: string): Omit<User, "id">[] {
-  if (env === "dev") {
-    return [
-      {
-        email: "victor@tutored.live",
-        cognitoSub: "aec9ea06-dee8-42da-9396-c82456858d1b",
-        createdAt: new Date(),
-        role: "ADMIN",
-        accountStatus: "ACTIVE",
-      },
-    ];
-  } else if (env === "staging") {
-    return [
-      {
-        email: "victor@tutored.live",
-        cognitoSub: "07155a34-6b7a-410d-9140-2af0b1877104",
-        createdAt: new Date(),
-        role: "ADMIN",
-        accountStatus: "ACTIVE",
-      },
-      {
-        email: "rafik@tutored.live",
-        cognitoSub: "8b43e5b1-268f-4530-9b11-944e5578a369",
-        createdAt: new Date(),
-        role: "ADMIN",
-        accountStatus: "ACTIVE",
-      },
-    ];
-  } else {
-    return [];
-  }
+async function createUsers(env: string): Promise<User[]> {
+  const devUsers: Prisma.UserCreateManyInput[] = [
+    {
+      email: "victor@tutored.live",
+      fullName: "Victor Merino",
+      cognitoSub: "aec9ea06-dee8-42da-9396-c82456858d1b",
+      createdAt: new Date(),
+      role: "ADMIN",
+      accountStatus: "ACTIVE",
+    },
+    {
+      email: "victor+mt@tutored.live",
+      fullName: "VM Mentor Teacher",
+      cognitoSub: "a5449f83-b175-42d2-bc0d-69ffbb039815",
+      createdAt: new Date(),
+      role: "MENTOR_TEACHER",
+      accountStatus: "ACTIVE",
+    },
+    {
+      email: "victor+tt@tutored.live",
+      fullName: "VM Tutor Teacher",
+      cognitoSub: "bfb7afbd-b5e4-4c8f-bb0f-9eba28d40882",
+      createdAt: new Date(),
+      role: "TUTOR_TEACHER",
+      accountStatus: "ACTIVE",
+    },
+    {
+      email: "victor+st@tutored.live",
+      fullName: "VM Substitute Teacher",
+      cognitoSub: "254598de-6867-4175-95f4-dcb03832ae3f",
+      createdAt: new Date(),
+      role: "TUTOR_TEACHER",
+      accountStatus: "ACTIVE",
+    },
+  ];
+
+  const stagingUsers: Prisma.UserCreateManyInput[] = [
+    {
+      email: "victor@tutored.live",
+      cognitoSub: "07155a34-6b7a-410d-9140-2af0b1877104",
+      createdAt: new Date(),
+      role: "ADMIN",
+      accountStatus: "ACTIVE",
+    },
+    {
+      email: "rafik@tutored.live",
+      cognitoSub: "8b43e5b1-268f-4530-9b11-944e5578a369",
+      createdAt: new Date(),
+      role: "ADMIN",
+      accountStatus: "ACTIVE",
+    },
+  ];
+
+  const users = env === "dev" ? devUsers : stagingUsers;
+
+  const results = await Promise.all(
+    users.map((devUser) => {
+      return prisma.user.create({
+        data: devUser,
+      });
+    })
+  );
+
+  console.log("[🌱 Seed] - Users created.");
+  return results;
 }
 
 /**
  * Orgs
  */
-function getOrgs(): Omit<Organization, "id">[] {
-  return [
-    {
-      name: "El Paso Organization",
+
+async function createOrgs(users: User[]) {
+  await createElPasoOrg(users);
+
+  console.log("[🌱 Seed] - Organizations created.");
+}
+
+async function createElPasoOrg(users: User[]) {
+  const startDate = new Date();
+  const endDate = new Date(startDate.getTime());
+  endDate.setFullYear(startDate.getFullYear() + 5);
+
+  console.log("startDate", startDate);
+  console.log("endDate", endDate);
+  const mentorTeacher = fromJust(
+    users.find((u) => u.email === "victor+mt@tutored.live")
+  );
+  const substituteTeacher = fromJust(
+    users.find((u) => u.email === "victor+st@tutored.live")
+  );
+  const tutorTeacher = fromJust(
+    users.find((u) => u.email === "victor+tt@tutored.live")
+  );
+
+  const newOrg = await prisma.organization.create({
+    data: {
+      name: "El Paso Org",
       location: "Texas",
-      description: "Org for schools in El Paso, TX.",
+      description: "Org for all schools in El Paso, TX.",
       district: "El Paso ISD",
       subDistrict: "El Paso ISD Socorro SD",
       createdAt: new Date(),
+      engagements: {
+        create: [
+          {
+            name: "EP - 5 Year",
+            startDate: startDate,
+            endDate: endDate,
+            cohorts: {
+              create: [
+                {
+                  createdAt: new Date(),
+                  name: "EP-G3 Cohort",
+                  grade: "3",
+                },
+                {
+                  createdAt: new Date(),
+                  name: "EP-G4 Cohort",
+                  grade: "4",
+                },
+                {
+                  createdAt: new Date(),
+                  name: "EP-G5 Cohort",
+                  grade: "5",
+                },
+              ],
+            },
+          },
+        ],
+      },
     },
-    {
-      name: "Short",
-      location: "Florida",
-      description: "Org for schools in Miami, FL.",
-      district: "ISD",
-      subDistrict: "SD",
-      createdAt: new Date(),
+    include: {
+      engagements: { include: { cohorts: true } },
     },
-    {
-      name: "An very long name for an organization.",
-      location: "Long Island",
-      description: "Test description",
-      district: "Also a very long school district name",
-      subDistrict:
-        "A mightly long sub district name that is even longer than the district name used above.",
-      createdAt: new Date(),
-    },
-  ];
+  });
+
+  // Staff Engagement & Cohorts
+  const engagement = newOrg.engagements[0];
+
+  await Promise.all([
+    prisma.engagementStaffAssignments.create({
+      data: {
+        engagement: { connect: { id: engagement.id } },
+        user: { connect: { id: mentorTeacher.id } },
+      },
+    }),
+    prisma.engagementStaffAssignments.create({
+      data: {
+        engagement: { connect: { id: engagement.id } },
+        user: { connect: { id: substituteTeacher.id } },
+      },
+    }),
+  ]);
+
+  await Promise.all([
+    prisma.cohortStaffAssignments.create({
+      data: {
+        cohort: { connect: { id: engagement.cohorts[0].id } },
+        user: { connect: { id: tutorTeacher.id } },
+      },
+    }),
+    prisma.cohortStaffAssignments.create({
+      data: {
+        cohort: { connect: { id: engagement.cohorts[1].id } },
+        user: { connect: { id: substituteTeacher.id } },
+      },
+    }),
+    prisma.cohortStaffAssignments.create({
+      data: {
+        cohort: { connect: { id: engagement.cohorts[2].id } },
+        user: { connect: { id: tutorTeacher.id } },
+      },
+    }),
+  ]);
 }
