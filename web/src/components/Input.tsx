f15895aab668885b769a2cf09c0375adcfcc875d
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-11 19:43:25 -0600

    add support for icon

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,15 +1,17 @@
+import clsx from "clsx";
 import { forwardRef } from "react";
 
 type Props = {
   id: string;
   name?: string;
-  label: string;
+  label?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type?: string;
   required?: boolean;
   description?: string;
   placeholder?: string;
+  leftIcon?: (props: React.ComponentProps<"svg">) => JSX.Element;
 };
 
 function InputForwardRef(
@@ -23,6 +25,7 @@ function InputForwardRef(
     required = false,
     description,
     placeholder: placeholderProp,
+    leftIcon: LeftIcon,
   }: Props,
   ref: React.Ref<HTMLInputElement>
 ) {
@@ -33,17 +36,34 @@ function InputForwardRef(
 
   return (
     <div>
-      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
-        {label}{" "}
-        {required && (
-          <span className="text-gray-500 text-sm font-light">*</span>
+      {label ? (
+        <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+          {label}{" "}
+          {required && (
+            <span className="text-gray-500 text-sm font-light">*</span>
+          )}
+        </label>
+      ) : (
+        // Screen reader only
+        <label htmlFor={id} className="sr-only">
+          {placeholderProp ?? "Input"}
+        </label>
+      )}
+
+      <div className="mt-1 relative">
+        {LeftIcon && (
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <LeftIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </div>
         )}
-      </label>
-      <div className="mt-1">
         <input
           id={id}
           type={type}
-          className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          className={clsx(
+            "shadow-sm block w-full sm:text-sm border-gray-300 rounded-md",
+            "focus:ring-blue-500 focus:border-blue-500",
+            LeftIcon && "pl-10"
+          )}
           aria-describedby={`${id}-input-description`}
           required={required}
           ref={ref}
