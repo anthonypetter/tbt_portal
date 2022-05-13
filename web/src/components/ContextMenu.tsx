406915dd20b9e0074388ecc97b891ba12b428820
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-13 14:05:08 -0600

    support disabled context menu items

--- web/src/components/ContextMenu.tsx
+++ web/src/components/ContextMenu.tsx
@@ -4,8 +4,8 @@ import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
 import clsx from "clsx";
 
 type Props = {
-  onClickEdit: () => void;
-  onClickDelete: () => void;
+  onClickEdit?: () => void;
+  onClickDelete?: () => void;
 };
 
 export function ContextMenu({ onClickEdit, onClickDelete }: Props) {
@@ -14,40 +14,66 @@ export function ContextMenu({ onClickEdit, onClickDelete }: Props) {
       <Menu.Items className="absolute z-10 right-0 mt-2 w-56 bg-white rounded-md focus:outline-none shadow-lg origin-top-right ring-1 ring-black ring-opacity-5">
         <div className="py-1">
           <Menu.Item>
-            {({ active }) => (
-              <button
-                onClick={onClickEdit}
-                className={clsx(
-                  active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                  "group flex items-center px-4 py-2 w-full text-sm"
-                )}
-              >
-                <PencilAltIcon
-                  className="mr-3 w-4 h-4 text-gray-400 group-hover:text-gray-500"
-                  aria-hidden="true"
-                />
-                Edit
-              </button>
-            )}
+            {({ active }) => {
+              const isDisabled = onClickEdit == null;
+
+              return (
+                <button
+                  disabled={isDisabled}
+                  className={clsx(
+                    "group flex items-center px-4 py-2 w-full text-sm",
+                    isDisabled
+                      ? "text-gray-300"
+                      : active
+                      ? "bg-gray-100 text-gray-900"
+                      : "text-gray-700"
+                  )}
+                  {...(onClickEdit ? { onClickEdit } : {})}
+                >
+                  <PencilAltIcon
+                    className={clsx(
+                      "mr-3 w-4 h-4",
+                      isDisabled
+                        ? "text-gray-300"
+                        : "text-gray-400 group-hover:text-gray-500"
+                    )}
+                    aria-hidden="true"
+                  />
+                  Edit
+                </button>
+              );
+            }}
           </Menu.Item>
           <Menu.Item>
-            {({ active }) => (
-              <button
-                onClick={onClickDelete}
-                className={clsx(
-                  active ? "bg-red-600 text-white" : "text-red-600",
-                  "group flex items-center px-4 py-2 w-full text-sm"
-                )}
-              >
-                <TrashIcon
+            {({ active }) => {
+              const isDisabled = onClickDelete == null;
+
+              return (
+                <button
+                  disabled={isDisabled}
                   className={clsx(
-                    "mr-3 w-4 h-4 text-red-600 group-hover:text-white"
+                    isDisabled
+                      ? "text-gray-300"
+                      : active
+                      ? "bg-red-600 text-white"
+                      : "text-red-600",
+                    "group flex items-center px-4 py-2 w-full text-sm"
                   )}
-                  aria-hidden="true"
-                />
-                Delete
-              </button>
-            )}
+                  {...(onClickDelete ? { onClickDelete } : {})}
+                >
+                  <TrashIcon
+                    className={clsx(
+                      "mr-3 w-4 h-4",
+                      isDisabled
+                        ? "text-gray-300"
+                        : "text-red-600 group-hover:text-white"
+                    )}
+                    aria-hidden="true"
+                  />
+                  Delete
+                </button>
+              );
+            }}
           </Menu.Item>
         </div>
       </Menu.Items>
