6d0357c548b51b2efd227c67f60954f805749abc
scottkuecker <kueckerscott@gmail.com>
2022-05-16 19:48:05 -0600

    add Avatar component

---
+++ web/src/components/Avatar.tsx
@@ -0,0 +1,8 @@
+import { UserCircleIcon } from "@heroicons/react/outline";
+import React from "react";
+
+export function Avatar() {
+  return (
+    <UserCircleIcon className="h-10 w-10 rounded-full" aria-hidden="true" />
+  );
+}
