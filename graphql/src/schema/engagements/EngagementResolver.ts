5285f6e33aa94bcd20592d4d2714532ddd8be8c8
scottkuecker <kueckerscott@gmail.com>
2022-05-19 08:54:38 -0600

    remove custom type

--- graphql/src/schema/engagements/EngagementResolver.ts
+++ graphql/src/schema/engagements/EngagementResolver.ts
@@ -1,24 +1,19 @@
 import { Context } from "../../context";
-import { EngagementService } from "@services/engagement";
-
-// TODO: This is the correct type, but maybe there's a better way to
-// extract this type from prisma.
-type EngagementParent = Awaited<
-  ReturnType<typeof EngagementService.getEngagements>
->[number];
+import { Engagement } from "../__generated__/graphql";
+import { parseId } from "../../utils/numbers";
 
 export const EngagementResolver = {
   async cohorts(
-    parent: EngagementParent,
+    parent: Engagement,
     _args: undefined,
     { authedUser, AuthorizationService, CohortService }: Context
   ) {
     AuthorizationService.assertIsAdmin(authedUser);
-    return CohortService.getCohorts(parent.id);
+    return CohortService.getCohorts(parseId(parent.id));
   },
 
   async staffAssignments(
-    parent: EngagementParent,
+    parent: Engagement,
     _args: undefined,
     { authedUser, AuthorizationService }: Context
   ) {
