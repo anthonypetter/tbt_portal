604cd8ccf6d9e30636d070598d862050151187de
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-19 10:33:29 -0600

    extract staff assignment utilities to enable reuse

--- graphql/src/schema/engagements/index.ts
+++ graphql/src/schema/engagements/index.ts
@@ -1,16 +1,10 @@
 import { gql } from "apollo-server";
 import { EngagementResolver } from "./EngagementResolver";
 import { Context } from "../../context";
-import {
-  MutationEditEngagementArgs,
-  NewStaffAssignment,
-} from "../__generated__/graphql";
+import { MutationEditEngagementArgs } from "../__generated__/graphql";
 import { parseId } from "../../utils/numbers";
-import { EngagementStaffAssignment } from "@prisma/client";
 import { fromJust } from "../../utils/types";
-import { EditInputStaffAssignment } from "../../services/engagement";
-import differenceBy from "lodash/differenceBy";
-import intersectionBy from "lodash/intersectionBy";
+import { calcStaffChanges } from "../../utils/staffAssignments";
 
 /**
  * Type Defs
@@ -67,10 +61,8 @@ async function editEngagement(
   { authedUser, AuthorizationService, EngagementService }: Context
 ) {
   AuthorizationService.assertIsAdmin(authedUser);
-  // We allow name to come in undefined (represents when the user is not trying to update the name field)
-  // But if name *does* come in, we don't allow it to be null.
-  // Not quite sure how to represent this in graphql schema yet.
-  if (input.name == null) {
+
+  if (input.name === null) {
     throw new Error("Engagement name cannot be null.");
   }
 
@@ -111,113 +103,3 @@ export const resolvers = {
   },
   Engagement: EngagementResolver,
 };
-
-/**
- * changes
- */
-
-type ChangeSet = {
-  additions: EditInputStaffAssignment[];
-  removals: EditInputStaffAssignment[];
-  updates: EditInputStaffAssignment[];
-};
-
-function calcStaffChanges(
-  existingAssignments: EngagementStaffAssignment[],
-  newAssignments: NewStaffAssignment[]
-): ChangeSet {
-  const existingStaff = existingAssignments.map((a) => fromExistingToInput(a));
-  const newStaff = newAssignments.map((a) => fromNewToInput(a));
-
-  return {
-    additions: findToAdd({ existingStaff, newStaff }),
-    removals: findToDelete({ existingStaff, newStaff }),
-    updates: findToUpdate({ existingStaff, newStaff }),
-  };
-}
-
-/**
- * These utilities help format incoming data to prisma formatted data.
- * They also allow us to compare existing vs incoming more easily.
- */
-
-function fromExistingToInput(
-  existingAssignment: EngagementStaffAssignment
-): EditInputStaffAssignment {
-  return {
-    userId: existingAssignment.userId,
-    assignmentRole: existingAssignment.assignmentRole,
-  };
-}
-
-function fromNewToInput(
-  newAssignment: NewStaffAssignment
-): EditInputStaffAssignment {
-  return {
-    userId: parseId(newAssignment.userId),
-    assignmentRole: newAssignment.assignmentRole,
-  };
-}
-
-function findToDelete({
-  existingStaff,
-  newStaff,
-}: {
-  existingStaff: EditInputStaffAssignment[];
-  newStaff: EditInputStaffAssignment[];
-}) {
-  const assignmentsToDelete = differenceBy(
-    existingStaff, //The array to inspect
-    newStaff, //The values to exclude
-    (teacher) => teacher.userId
-  );
-
-  return assignmentsToDelete;
-}
-
-function findToAdd({
-  existingStaff,
-  newStaff,
-}: {
-  existingStaff: EditInputStaffAssignment[];
-  newStaff: EditInputStaffAssignment[];
-}) {
-  const assignmentsToAdd = differenceBy(
-    newStaff, //The array to inspect
-    existingStaff, //The values to exclude
-    (teacher) => teacher.userId
-  );
-
-  return assignmentsToAdd;
-}
-
-function findToUpdate({
-  existingStaff,
-  newStaff,
-}: {
-  existingStaff: EditInputStaffAssignment[];
-  newStaff: EditInputStaffAssignment[];
-}) {
-  const staffInBothArrays = intersectionBy(
-    newStaff,
-    existingStaff,
-    (teacher) => teacher.userId
-  );
-
-  //Now lets figure out if their role changed.
-  const assignmentsToUpdate = staffInBothArrays.filter((assignment) => {
-    const existingTeacher = fromJust(
-      existingStaff.find((t) => t.userId === assignment.userId),
-      "existingTeacher"
-    );
-
-    const newTeacher = fromJust(
-      newStaff.find((t) => t.userId === assignment.userId),
-      "newTeacher"
-    );
-
-    return existingTeacher.assignmentRole !== newTeacher.assignmentRole;
-  });
-
-  return assignmentsToUpdate;
-}
