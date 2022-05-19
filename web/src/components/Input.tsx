8d467090b824f79bc432bc9a42f08bdff9d4c512
scottkuecker <kueckerscott@gmail.com>
2022-05-18 18:31:20 -0600

    apply margin only when label is present

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -56,7 +56,7 @@ function InputForwardRef(
         </label>
       )}
 
-      <div className="mt-1 relative">
+      <div className={clsx(label && "mt-1", "relative")}>
         {LeftIcon && (
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <LeftIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
