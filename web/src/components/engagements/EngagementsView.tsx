7ea985b54f58de432c13e110e7385f4120bd5283
scottkuecker <kueckerscott@gmail.com>
2022-05-20 10:28:21 -0600

    fix crashing bug and add error boundary

--- web/src/components/engagements/EngagementsView.tsx
+++ web/src/components/engagements/EngagementsView.tsx
@@ -1,7 +1,6 @@
 import { gql } from "@apollo/client";
 import { OrgDetailPageEngagementsQuery } from "@generated/graphql";
 import { PlusIcon, SearchIcon } from "@heroicons/react/outline";
-import clsx from "clsx";
 import { DateText } from "components/Date";
 import { Input } from "components/Input";
 import { useState } from "react";
@@ -11,6 +10,8 @@ import { AssignmentRoleBadge } from "components/AssignmentRoleBadge";
 import { Button } from "components/Button";
 import { DetailsAside } from "components/DetailsAside";
 import { AddEngagementModal } from "./AddNewEngagementModal";
+import { ErrorBoundary } from "components/ErrorBoundary";
+import { ErrorBox } from "components/ErrorBox";
 
 EngagementsView.fragments = {
   engagementsList: gql`
@@ -66,57 +67,53 @@ export function EngagementsView({ organization }: Props) {
     filteredEngagements.find((e) => e.id === selectedEngagementId) ?? null;
 
   return (
-    <>
+    <ErrorBoundary fallbackRender={() => <ErrorBox className="mt-4" />}>
       <div className="flex min-h-[500px]">
-        <div className={clsx("flex-1 flex flex-col overflow-hidden")}>
-          <div className="flex-1 flex items-stretch overflow-hidden">
-            <main className="flex-1 overflow-y-auto">
-              <div className="flex my-4 justify-between">
-                <div className="flex-1 lg:max-w-sm lg:mr-2 lg:ml-1">
-                  <Input
-                    id="engagements-search"
-                    type="search"
-                    placeholder="Search"
-                    leftIcon={SearchIcon}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                  />
-                </div>
-                <Button
-                  type="button"
-                  theme="tertiary"
-                  className="mx-2"
-                  onClick={() => setShowAddModal(true)}
-                >
-                  <PlusIcon
-                    className="-ml-2 mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  <span>Add</span>
-                </Button>
-              </div>
-
-              <EngagementsTable
-                engagements={filteredEngagements}
-                onRowClick={(id) => setSelectedEngagementId(id)}
-                selectedEngagement={selectedEngagement}
+        <main className="flex-1">
+          <div className="flex justify-between my-4">
+            <div className="flex-1 lg:max-w-sm lg:mr-2 lg:ml-1">
+              <Input
+                id="engagements-search"
+                type="search"
+                placeholder="Search"
+                leftIcon={SearchIcon}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
-
-              <AddEngagementModal
-                organizationId={organization.id}
-                show={showAddModal}
-                onCancel={() => setShowAddModal(false)}
-                onSuccess={() => setShowAddModal(false)}
+            </div>
+            <Button
+              type="button"
+              theme="tertiary"
+              className="mx-2"
+              onClick={() => setShowAddModal(true)}
+            >
+              <PlusIcon
+                className="-ml-2 mr-1 h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
-            </main>
-
-            <DetailsSidebar
-              selectedEngagement={selectedEngagement}
-              onClose={() => setSelectedEngagementId(null)}
-            />
+              <span>Add</span>
+            </Button>
           </div>
-        </div>
+
+          <EngagementsTable
+            engagements={filteredEngagements}
+            onRowClick={(id) => setSelectedEngagementId(id)}
+            selectedEngagement={selectedEngagement}
+          />
+
+          <AddEngagementModal
+            organizationId={organization.id}
+            show={showAddModal}
+            onCancel={() => setShowAddModal(false)}
+            onSuccess={() => setShowAddModal(false)}
+          />
+        </main>
+
+        <DetailsSidebar
+          selectedEngagement={selectedEngagement}
+          onClose={() => setSelectedEngagementId(null)}
+        />
       </div>
-    </>
+    </ErrorBoundary>
   );
 }
 
