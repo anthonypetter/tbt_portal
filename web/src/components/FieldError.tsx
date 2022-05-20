9932df2cc2fcdda4798e24fb3e6363d7f1ef91c6
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-20 14:51:32 -0600

    prevent icon from being squeezed to death

--- web/src/components/FieldError.tsx
+++ web/src/components/FieldError.tsx
@@ -4,7 +4,7 @@ export function FieldError({ msg }: { msg: string }) {
   return (
     <span className="mt-1 flex items-center text-sm text-red-500">
       <ExclamationCircleIcon
-        className="w-4 h-4 text-red-400 mr-2"
+        className="w-4 h-4 text-red-400 mr-2 flex-shrink-0"
         aria-hidden="true"
       />
       {msg}
