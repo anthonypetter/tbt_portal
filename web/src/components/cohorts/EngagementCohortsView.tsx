17e5d70aa30a00de7f4b5c5a788ae6f44faf74d9
scottkuecker <kueckerscott@gmail.com>
2022-05-24 16:23:37 -0600

    add cohort modal to engagement cohorts view

--- web/src/components/cohorts/EngagementCohortsView.tsx
+++ web/src/components/cohorts/EngagementCohortsView.tsx
@@ -1,7 +1,6 @@
 import { gql } from "@apollo/client";
 import { EngagementDetailPageQuery } from "@generated/graphql";
-import { SearchIcon } from "@heroicons/react/outline";
-import clsx from "clsx";
+import { PlusIcon, SearchIcon } from "@heroicons/react/outline";
 import { DateText } from "components/Date";
 import { Input } from "components/Input";
 import { useState } from "react";
@@ -9,6 +8,10 @@ import { CohortsTable } from "./CohortsTable";
 import filter from "lodash/filter";
 import { DetailsAside } from "components/DetailsAside";
 import { AssignmentRoleBadge } from "components/AssignmentRoleBadge";
+import { Button } from "components/Button";
+import { AddNewCohortModal } from "./AddNewCohortModal";
+import { ErrorBox } from "components/ErrorBox";
+import { ErrorBoundary } from "components/ErrorBoundary";
 
 EngagementCohortsView.fragments = {
   cohortsList: gql`
@@ -47,6 +50,7 @@ type Props = {
 export function EngagementCohortsView({ engagement }: Props) {
   const [selectedCohortId, setSelectedCohortId] = useState<string | null>(null);
   const [searchTerm, setSearchTerm] = useState<string | null>(null);
+  const [showAddModal, setShowAddModal] = useState(false);
 
   const filteredCohorts = searchTerm
     ? filter(engagement.cohorts, (c) =>
@@ -58,11 +62,11 @@ export function EngagementCohortsView({ engagement }: Props) {
     filteredCohorts.find((e) => e.id === selectedCohortId) ?? null;
 
   return (
-    <div className="flex min-h-full">
-      <div className={clsx("flex-1 flex flex-col overflow-hidden")}>
-        <div className="flex-1 flex items-stretch overflow-hidden">
-          <main className="flex-1 overflow-y-auto">
-            <div className="flex-1 my-4 lg:max-w-sm lg:mr-2">
+    <ErrorBoundary fallbackRender={() => <ErrorBox className="mt-4" />}>
+      <div className="flex min-h-[500px]">
+        <main className="flex-1">
+          <div className="flex justify-between my-4">
+            <div className="flex-1 lg:max-w-sm lg:mr-2 lg:ml-1">
               <Input
                 id="cohorts-search"
                 type="search"
@@ -71,22 +75,41 @@ export function EngagementCohortsView({ engagement }: Props) {
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
             </div>
+            <Button
+              type="button"
+              theme="tertiary"
+              className="mx-2"
+              onClick={() => setShowAddModal(true)}
+            >
+              <PlusIcon
+                className="-ml-2 mr-1 h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              <span>Add</span>
+            </Button>
+          </div>
 
-            <CohortsTable
-              organizationId={engagement.organization.id}
-              cohorts={filteredCohorts}
-              onRowClick={(id) => setSelectedCohortId(id)}
-              selectedCohort={selectedCohort}
-            />
-          </main>
-
-          <DetailsSidebar
+          <CohortsTable
+            organizationId={engagement.organization.id}
+            cohorts={filteredCohorts}
+            onRowClick={(id) => setSelectedCohortId(id)}
             selectedCohort={selectedCohort}
-            onClose={() => setSelectedCohortId(null)}
           />
-        </div>
+
+          <AddNewCohortModal
+            engagementId={engagement.id}
+            show={showAddModal}
+            onCancel={() => setShowAddModal(false)}
+            onSuccess={() => setShowAddModal(false)}
+          />
+        </main>
+
+        <DetailsSidebar
+          selectedCohort={selectedCohort}
+          onClose={() => setSelectedCohortId(null)}
+        />
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
