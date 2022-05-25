e8ce7d9e65e6120ca037320786dfe0a90f2666fc
anthonypetter <anthonypsalazar9@gmail.com>
2022-05-24 19:10:05 -0600

    improve add org modal

--- web/src/components/organizations/AddOrgModal.tsx
+++ web/src/components/organizations/AddOrgModal.tsx
@@ -1,13 +1,12 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { Spinner } from "../Spinner";
 import { Modal } from "../Modal";
 import { ErrorBox } from "components/ErrorBox";
-import { gql, useMutation } from "@apollo/client";
+import { ApolloError, gql, useMutation } from "@apollo/client";
 import { AddOrganizationMutation } from "@generated/graphql";
 import { fromJust } from "@utils/types";
 import { FaRegBuilding } from "react-icons/fa";
 import { Input } from "components/Input";
-import { triggerSuccessToast } from "components/Toast";
 
 const ADD_ORGANIZATION = gql`
   mutation AddOrganization($input: AddOrganizationInput!) {
@@ -20,105 +19,16 @@ const ADD_ORGANIZATION = gql`
   }
 `;
 
-export function AddOrgModal({
-  show,
-  onCancel,
-  onSuccess,
-}: {
+type Props = {
   show: boolean;
-  onCancel: () => void;
-  onSuccess: () => void;
-}) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const cancelButtonRef = useRef(null);
+  closeModal: () => void;
+};
 
-  // Form state
-  const [name, setName] = useState<string | null | undefined>(undefined);
-  const [district, setDistrict] = useState<string | null | undefined>(
-    undefined
-  );
-  const [subDistrict, setSubDistrict] = useState<string | null | undefined>(
-    undefined
-  );
-
-  const [addOrg] = useMutation<AddOrganizationMutation>(ADD_ORGANIZATION, {
-    update(cache, { data }) {
-      if (!data?.addOrganization) {
-        return;
-      }
-
-      cache.modify({
-        fields: {
-          organizations(existingOrgs = []) {
-            const newOrgRef = cache.writeFragment({
-              data: data.addOrganization,
-              fragment: gql`
-                fragment NewOrg on Organization {
-                  id
-                  name
-                  district
-                  subDistrict
-                  __typename
-                }
-              `,
-            });
-            return [...existingOrgs, newOrgRef];
-          },
-        },
-      });
-    },
-  });
-
-  useEffect(() => {
-    const resetForm = () => {
-      setLoading(false);
-      setError(null);
-      setName(undefined);
-      setDistrict(undefined);
-      setSubDistrict(undefined);
-    };
-
-    if (show) {
-      resetForm();
-    }
-  }, [show]);
-
-  const onAddOrg = async () => {
-    setLoading(true);
-
-    try {
-      const { data } = await addOrg({
-        variables: {
-          input: { name: fromJust(name, "name"), district, subDistrict },
-        },
-      });
-
-      if (!data?.addOrganization) {
-        setLoading(false);
-        throw new Error("Something went wrong with the invite.");
-      }
-
-      setLoading(false);
-      triggerSuccessToast({ message: "Organization successfully created!" });
-      onSuccess();
-    } catch (error: unknown) {
-      console.error(error);
-
-      const message =
-        error instanceof Error
-          ? error.message
-          : "Something went wrong with the invite.";
-
-      setLoading(false);
-      setError(message);
-    }
-  };
-
+export function AddOrgModal({ show, closeModal }: Props) {
   return (
     <Modal
       show={show}
-      onClose={onCancel}
+      onClose={closeModal}
       icon={
         <div className="flex flex-shrink-0 items-center justify-center mx-auto w-12 h-12 bg-green-100 rounded-full sm:mx-0 sm:w-10 sm:h-10">
           <FaRegBuilding
@@ -129,42 +39,114 @@ export function AddOrgModal({
       }
       title="Add an organization"
     >
-      <div className="py-3">
-        <div className="mb-4">
-          <div className="space-y-4">
-            <Input
-              id="org-name"
-              label="Name"
-              value={name ?? ""}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-
-            <Input
-              id="org-district-name"
-              label="District"
-              value={district ?? ""}
-              onChange={(e) => setDistrict(e.target.value)}
-            />
-
-            <Input
-              id="org-sub-district-name"
-              label="SubDistrict"
-              value={subDistrict ?? ""}
-              onChange={(e) => setSubDistrict(e.target.value)}
-            />
-          </div>
-        </div>
-        {error && <ErrorBox />}
-        <Modal.Buttons>
-          <Modal.Button type="confirm" onClick={onAddOrg} disabled={!name}>
-            {loading ? <Spinner /> : "Add"}
-          </Modal.Button>
-          <Modal.Button type="cancel" onClick={onCancel} ref={cancelButtonRef}>
-            Cancel
-          </Modal.Button>
-        </Modal.Buttons>
-      </div>
+      <AddOrgModaBody onCancel={closeModal} onSuccess={closeModal} />
     </Modal>
+  );
+}
+
+type AddOrgModaBodyProps = {
+  onCancel: () => void;
+  onSuccess: () => void;
+};
+
+export function AddOrgModaBody({ onCancel, onSuccess }: AddOrgModaBodyProps) {
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const cancelButtonRef = useRef(null);
+  const [name, setName] = useState<string | null | undefined>();
+  const [description, setDescription] = useState<string | null | undefined>();
+  const [district, setDistrict] = useState<string | null | undefined>();
+  const [subDistrict, setSubDistrict] = useState<string | null | undefined>();
+
+  const [addOrg, { loading }] = useMutation<AddOrganizationMutation>(
+    ADD_ORGANIZATION,
+    {
+      onError: (err: ApolloError) => setErrorMsg(err.message),
+      onCompleted: onSuccess,
+
+      update(cache, { data }) {
+        if (!data?.addOrganization) {
+          return;
+        }
+
+        cache.modify({
+          fields: {
+            organizations(existingOrgs = []) {
+              const newOrgRef = cache.writeFragment({
+                data: data.addOrganization,
+                fragment: gql`
+                  fragment NewOrg on Organization {
+                    id
+                    name
+                    district
+                    subDistrict
+                    __typename
+                  }
+                `,
+              });
+              return [...existingOrgs, newOrgRef];
+            },
+          },
+        });
+      },
+    }
+  );
+
+  const onAddOrg = async () => {
+    await addOrg({
+      variables: {
+        input: {
+          name: fromJust(name, "name"),
+          description,
+          district,
+          subDistrict,
+        },
+      },
+    });
+  };
+
+  return (
+    <div className="py-3">
+      <div className="mb-4">
+        <div className="space-y-4">
+          <Input
+            id="org-name"
+            label="Name"
+            value={name ?? ""}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+
+          <Input
+            id="org-description"
+            label="Description"
+            value={description ?? ""}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <Input
+            id="org-district-name"
+            label="District"
+            value={district ?? ""}
+            onChange={(e) => setDistrict(e.target.value)}
+          />
+
+          <Input
+            id="org-sub-district-name"
+            label="SubDistrict"
+            value={subDistrict ?? ""}
+            onChange={(e) => setSubDistrict(e.target.value)}
+          />
+        </div>
+      </div>
+      {errorMsg && <ErrorBox msg={errorMsg} />}
+      <Modal.Buttons>
+        <Modal.Button type="confirm" onClick={onAddOrg} disabled={!name}>
+          {loading ? <Spinner /> : "Add"}
+        </Modal.Button>
+        <Modal.Button type="cancel" onClick={onCancel} ref={cancelButtonRef}>
+          Cancel
+        </Modal.Button>
+      </Modal.Buttons>
+    </div>
   );
 }
