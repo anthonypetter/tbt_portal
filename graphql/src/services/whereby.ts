6cc4a73ddb97f1c97a7fba9cf83837de504b8cef
scottkuecker <kueckerscott@gmail.com>
2022-06-02 10:48:13 -0700

    graphql: replaced axios with node-fetch to keep consitent with web-frontend          also applied prettier lint

--- graphql/src/services/whereby.ts
+++ graphql/src/services/whereby.ts
@@ -1,38 +1,45 @@
-import axios from "axios"
-import { WHEREBY_URL, WHEREBY_API_KEY } from '../config'
+import fetch from "node-fetch";
+import { WHEREBY_URL, WHEREBY_API_KEY } from "../config";
 
 const headers = {
-    Authorization: `Bearer ${WHEREBY_API_KEY}`,
-    "Content-Type": "application/json",
-}
+  Authorization: `Bearer ${WHEREBY_API_KEY}`,
+  "Content-Type": "application/json",
+};
 
 async function createWhereByRoom(startDate: string, endDate: string) {
-    try {
-        const data = {
-            startDate,
-            endDate,
-            isLocked: true,
-            roomMode: "group",
-            fields: ["hostRoomUrl"],
-        }
-        const result = await axios.post(WHEREBY_URL, data, { headers })
-        return result.data
-    } catch (error) {
-        throw error
-    }
+  try {
+    const data = {
+      startDate,
+      endDate,
+      isLocked: true,
+      roomMode: "group",
+      fields: ["hostRoomUrl"],
+    };
+    const result = await fetch(WHEREBY_URL, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data),
+    });
+    return await result.json();
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function deleteWhereByRoom(meetingId: string | null) {
-    if (!meetingId) return
-    try {
-        const result = await axios.delete(`${WHEREBY_URL}/${meetingId}`, {headers})
-        return  result.data
-    } catch (error) {
-        throw error
-    }
+  if (!meetingId) return;
+  try {
+    const result = await fetch(`${WHEREBY_URL}/${meetingId}`, {
+      method: "DELETE",
+      headers,
+    });
+    return await result.json();
+  } catch (error) {
+    throw error;
+  }
 }
 
 export const WhereByService = {
-    createWhereByRoom,
-    deleteWhereByRoom
-}
+  createWhereByRoom,
+  deleteWhereByRoom,
+};
