4a9402f52ab7cd815c63581521ae3b7b876514e1
anthonypetter <anthonypsalazar9@gmail.com>
2022-06-01 19:28:30 -0600

    web: fix typo in assignment subject badge

--- web/src/components/AssignmentSubjectBadge.tsx
+++ web/src/components/AssignmentSubjectBadge.tsx
@@ -10,15 +10,13 @@ type Props = {
 export function AssignmentSubjectBadge({ subject }: Props) {
   switch (subject) {
     case AssignmentSubject.Math:
-      return (
-        <Badge className="text-purple-800 bg-purple-100 mr-5">General</Badge>
-      );
+      return <Badge className="text-purple-800 bg-purple-100 mr-5">Math</Badge>;
 
     case AssignmentSubject.Ela:
       return <Badge className="text-green-800 bg-green-100 mr-5">ELA</Badge>;
 
     case AssignmentSubject.General:
-      return <Badge className="text-blue-800 bg-blue-100 mr-5">Math</Badge>;
+      return <Badge className="text-blue-800 bg-blue-100 mr-5">General</Badge>;
 
     default:
       assertUnreachable(subject);
