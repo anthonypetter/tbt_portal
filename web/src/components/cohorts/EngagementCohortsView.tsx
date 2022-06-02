3b5bf952719082f9bee8d240cf65752406cac69c
scottkuecker <kueckerscott@gmail.com>
2022-06-01 19:58:15 -0600

    web: use cohort staff assignments to display subject properly

--- web/src/components/cohorts/EngagementCohortsView.tsx
+++ web/src/components/cohorts/EngagementCohortsView.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { CohortsTable } from "./CohortsTable";
 import filter from "lodash/filter";
 import { DetailsAside } from "components/DetailsAside";
-import { AssignmentRoleBadge } from "components/AssignmentRoleBadge";
+import { AssignmentSubjectBadge } from "components/AssignmentSubjectBadge";
 import { Button } from "components/Button";
 import { AddNewCohortModal } from "./AddNewCohortModal";
 import { ErrorBox } from "components/ErrorBox";
@@ -34,7 +34,7 @@ EngagementCohortsView.fragments = {
             fullName
             email
           }
-          assignmentRole
+          subject
         }
       }
     }
@@ -159,11 +159,7 @@ function DetailsSidebar({ selectedCohort, onClose }: DetailsSidebarProps) {
             <DetailsAside.Line
               key={assignment.user.id}
               label={assignment.user.fullName}
-              value={
-                <AssignmentRoleBadge
-                  assignmentRole={assignment.assignmentRole}
-                />
-              }
+              value={<AssignmentSubjectBadge subject={assignment.subject} />}
             />
           ))
         )}
