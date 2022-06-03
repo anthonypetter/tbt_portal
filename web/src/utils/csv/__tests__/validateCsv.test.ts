b835b914dce3e811a2c872cbd3d611adbedf3984
scottkuecker <kueckerscott@gmail.com>
2022-06-02 19:10:22 -0600

    web: add optional column validation test

--- web/src/utils/csv/__tests__/validateCsv.test.ts
+++ web/src/utils/csv/__tests__/validateCsv.test.ts
@@ -1,46 +1,43 @@
-import {
-  validateCsv,
-  CsvValidationError,
-  validateCsvFile,
-} from "../validateCsv";
+import { validateCsv, validateCsvFile } from "../validateCsv";
 import fs from "fs";
 import path from "path";
+import { CsvValidationErrorMessage } from "@utils/errors";
 
 describe("csvValidation", () => {
   test("should detect invalid format for null", () => {
     const { errors } = validateCsv(null);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.invalidFormat);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for undefined", () => {
     const { errors } = validateCsv(undefined);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.invalidFormat);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for empty object", () => {
     const { errors } = validateCsv({});
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.invalidFormat);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for random string", () => {
     const { errors } = validateCsv("random string");
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.invalidFormat);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for random number", () => {
     const { errors } = validateCsv(1234);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.invalidFormat);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect empty array", () => {
     const { errors } = validateCsv([]);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.emptyData);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.emptyData);
   });
 
   test("should detect empty CSV", async () => {
@@ -50,7 +47,7 @@ describe("csvValidation", () => {
 
     const { errors } = await validateCsvFile(readstream);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.emptyData);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.emptyData);
   });
 
   test("should detect missing header", async () => {
@@ -60,20 +57,20 @@ describe("csvValidation", () => {
 
     const { errors } = await validateCsvFile(readstream);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.rowLengthMismatch);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.rowLengthMismatch);
   });
 
-  test("should detect row column mismatch.", async () => {
+  test("should detect row column mismatch", async () => {
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-row-column-mismatch.csv")
     );
 
     const { errors } = await validateCsvFile(readstream);
     expect(errors.length).toBeGreaterThan(0);
-    expect(errors[0].message).toBe(CsvValidationError.rowLengthMismatch);
+    expect(errors[0].message).toBe(CsvValidationErrorMessage.rowLengthMismatch);
   });
 
-  test("should detect a missing column due to typo.", async () => {
+  test("should detect a missing column due to typo", async () => {
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-header-typo.csv")
     );
@@ -81,11 +78,11 @@ describe("csvValidation", () => {
     const { errors } = await validateCsvFile(readstream);
     expect(errors.length).toBeGreaterThan(0);
     const error = errors[0];
-    expect(error.message).toBe(CsvValidationError.missingRequiredColumn);
-    expect(error.hint).toBe("cohort");
+    expect(error.message).toBe(CsvValidationErrorMessage.missingRequiredColumn);
+    expect(error.hint).toBe("Columns: cohort");
   });
 
-  test("should detect multiple missing columns.", async () => {
+  test("should detect multiple missing columns", async () => {
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-missing-required-columns.csv")
     );
@@ -93,7 +90,16 @@ describe("csvValidation", () => {
     const { errors } = await validateCsvFile(readstream);
     expect(errors.length).toBeGreaterThan(0);
     const error = errors[0];
-    expect(error.message).toBe(CsvValidationError.missingRequiredColumn);
-    expect(error.hint).toBe("grade, monday, tuesday, wednesday");
+    expect(error.message).toBe(CsvValidationErrorMessage.missingRequiredColumn);
+    expect(error.hint).toBe("Columns: grade, monday, tuesday, wednesday");
+  });
+
+  test("should passing validation when optional columns are missing", async () => {
+    const readstream = fs.createReadStream(
+      path.resolve(__dirname, "./valid-missing-optional-columns.csv")
+    );
+
+    const { errors } = await validateCsvFile(readstream);
+    expect(errors.length).toEqual(0);
   });
 });
