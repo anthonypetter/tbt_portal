5df32f9bb8e4b2608d3faacd1ee70a5f81c77573
scottkuecker <kueckerscott@gmail.com>
2022-06-07 15:12:50 -0600

    web: fix query name

--- web/src/components/cohorts/constants.ts
+++ web/src/components/cohorts/constants.ts
@@ -1,2 +1,2 @@
-export const ENGAGEMENT_DETAIL_PAGE_QUERY_NAME = "EngagementDetailPage";
+export const ENGAGEMENT_DETAIL_PAGE_QUERY_NAME = "EngagementDetailsPage";
 export const ORG_DETAIL_PAGE_COHORTS_NAME = "OrgDetailPageCohorts";
