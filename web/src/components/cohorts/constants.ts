753e92ebca632927bc04975848f6da5a4a1c985a
scottkuecker <kueckerscott@gmail.com>
2022-06-07 16:35:50 -0600

    web: fix constant variable name for consistency

--- web/src/components/cohorts/constants.ts
+++ web/src/components/cohorts/constants.ts
@@ -1,2 +1,2 @@
-export const ENGAGEMENT_DETAIL_PAGE_QUERY_NAME = "EngagementDetailsPage";
+export const ENGAGEMENT_DETAILS_PAGE_QUERY_NAME = "EngagementDetailsPage";
 export const ORG_DETAIL_PAGE_COHORTS_NAME = "OrgDetailPageCohorts";
