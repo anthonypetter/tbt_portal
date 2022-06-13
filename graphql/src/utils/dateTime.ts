1d4e4efea99c4ccb0cf135ac0bcb7f00671caf7d
scottkuecker <kueckerscott@gmail.com>
2022-06-13 17:36:58 -0600

    graphql: improve comment explaining use of date extraction util

--- graphql/src/utils/dateTime.ts
+++ graphql/src/utils/dateTime.ts
@@ -1,8 +1,11 @@
 /**
- * Given a particular DateTime, this function extract's that DateTime's date
+ * Given a particular DateTime, this function extracts the DateTime's date and sets
+ * hours, minutes, and seconds to 0.
  *
- * The DB does not store times for engagement and cohort start/end dates.
+ * Since the DB does not store times for engagement and cohort start/end dates, using
+ * a date with no time simplifies date comparisons in database queries.
  */
+
 export function extractDateFromDateTime(originalDateTime: Date) {
   const date = new Date(originalDateTime.getTime());
 
