aa079bd26ce72736e764db08899b2bb64138f0b8
scottkuecker <kueckerscott@gmail.com>
2022-06-17 12:57:54 -0400

    web: removed unnecessary fragment from AuthedLayout - mostly whitespace changes * I had the option to remove the plain <div> instead, but decided against it. For such a large component switching to a fragment might make the DOM more cluttered for no gain

--- web/src/components/AuthedLayout.tsx
+++ web/src/components/AuthedLayout.tsx
@@ -169,204 +169,202 @@ export function AuthedLayout({ children }: Props) {
   const navigation = getNavigation(user.role, router.pathname);
 
   return (
-    <>
-      <div>
-        {/* Pop-open sidebar for mobile */}
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="fixed inset-0 flex z-40 md:hidden"
-            onClose={setSidebarOpen}
+    <div>
+      {/* Pop-open sidebar for mobile */}
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 flex z-40 md:hidden"
+          onClose={setSidebarOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div
+              className={`relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 ${sidebar.mainBackground}`}
             >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <div
-                className={`relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 ${sidebar.mainBackground}`}
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
               >
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XIcon
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </Transition.Child>
-                <div className="flex-shrink-0 flex items-center px-4">
-                  <CheckmarkTitle />
-                </div>
-                <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                  <nav className="px-2 space-y-1">
-                    {navigation.map((item) => (
-                      <Link href={item.href} key={item.name}>
-                        <a
-                          className={clsx(
-                            item.current
-                              ? `${sidebar.activeLinkBackground} ${sidebar.activeLinkText}`
-                              : `${sidebar.inactiveLinkText} ${sidebar.inactiveLinkBackgroundHover}`,
-                            "group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                          )}
-                        >
-                          <item.icon
-                            className={clsx(
-                              "mr-4 flex-shrink-0 h-6 w-6",
-                              item.current
-                                ? sidebar.activeIconColor
-                                : sidebar.inactiveIconColor
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      </Link>
-                    ))}
-                  </nav>
-                </div>
-              </div>
-            </Transition.Child>
-            <div className="flex-shrink-0 w-14" aria-hidden="true">
-              {/* Dummy element to force sidebar to shrink to fit close icon */}
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        {/* Static sidebar for desktop */}
-        <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div
-            className={`flex flex-col flex-grow pt-5 ${sidebar.mainBackground} overflow-y-auto`}
-          >
-            <div className="flex items-center flex-shrink-0 px-4">
-              <CheckmarkTitle />
-            </div>
-            <div className="mt-5 flex-1 flex flex-col">
-              <nav className="flex-1 px-2 pb-4 space-y-1">
-                {navigation.map((item) => (
-                  <Link href={item.href} key={item.name}>
-                    <a
-                      className={clsx(
-                        item.current
-                          ? `${sidebar.activeLinkBackground} ${sidebar.activeLinkText}`
-                          : `${sidebar.inactiveLinkText} ${sidebar.inactiveLinkBackgroundHover}`,
-                        "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                      )}
-                    >
-                      <item.icon
-                        className={clsx(
-                          "mr-3 flex-shrink-0 h-6 w-6",
-                          item.current
-                            ? sidebar.activeIconColor
-                            : sidebar.inactiveIconColor
-                        )}
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  </Link>
-                ))}
-              </nav>
-            </div>
-          </div>
-        </div>
-
-        {/* Top bar + Content container */}
-        <div className="md:pl-64 flex flex-col flex-1">
-
-          {/* Top bar container */}
-          <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
-
-            {/* Mobile Hamburger button */}
-            <button
-              type="button"
-              className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 md:hidden"
-              onClick={() => setSidebarOpen(true)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-
-            {/* Top bar */}
-            <div className="flex-1 px-4 flex justify-between">
-              {/* Search input */}
-              <div className="flex-1 flex">
-                <form className="w-full flex md:ml-0" action="#" method="GET">
-                  <label htmlFor="search-field" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                    <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                      <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                    </div>
-                    <input
-                      id="search-field"
-                      className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                      placeholder="Search"
-                      type="search"
-                      name="search"
+                <div className="absolute top-0 right-0 -mr-12 pt-2">
+                  <button
+                    type="button"
+                    className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XIcon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
                     />
-                  </div>
-                </form>
+                  </button>
+                </div>
+              </Transition.Child>
+              <div className="flex-shrink-0 flex items-center px-4">
+                <CheckmarkTitle />
               </div>
-
-              {/* Utility Buttons */}
-              <div className="ml-4 flex items-center md:ml-6">
-                <button
-                  type="button"
-                  className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                <AvatarDropdown />
+              <div className="mt-5 flex-1 h-0 overflow-y-auto">
+                <nav className="px-2 space-y-1">
+                  {navigation.map((item) => (
+                    <Link href={item.href} key={item.name}>
+                      <a
+                        className={clsx(
+                          item.current
+                            ? `${sidebar.activeLinkBackground} ${sidebar.activeLinkText}`
+                            : `${sidebar.inactiveLinkText} ${sidebar.inactiveLinkBackgroundHover}`,
+                          "group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                        )}
+                      >
+                        <item.icon
+                          className={clsx(
+                            "mr-4 flex-shrink-0 h-6 w-6",
+                            item.current
+                              ? sidebar.activeIconColor
+                              : sidebar.inactiveIconColor
+                          )}
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
+                    </Link>
+                  ))}
+                </nav>
               </div>
             </div>
+          </Transition.Child>
+          <div className="flex-shrink-0 w-14" aria-hidden="true">
+            {/* Dummy element to force sidebar to shrink to fit close icon */}
           </div>
+        </Dialog>
+      </Transition.Root>
 
-          {/* Content area */}
-          <main className="z-0 h-screen-nav overflow-y-scroll">
-            <div className="pt-6 pb-20">
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                {children}
-              </div>
-            </div>
-          </main>
+      {/* Static sidebar for desktop */}
+      <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div
+          className={`flex flex-col flex-grow pt-5 ${sidebar.mainBackground} overflow-y-auto`}
+        >
+          <div className="flex items-center flex-shrink-0 px-4">
+            <CheckmarkTitle />
+          </div>
+          <div className="mt-5 flex-1 flex flex-col">
+            <nav className="flex-1 px-2 pb-4 space-y-1">
+              {navigation.map((item) => (
+                <Link href={item.href} key={item.name}>
+                  <a
+                    className={clsx(
+                      item.current
+                        ? `${sidebar.activeLinkBackground} ${sidebar.activeLinkText}`
+                        : `${sidebar.inactiveLinkText} ${sidebar.inactiveLinkBackgroundHover}`,
+                      "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    )}
+                  >
+                    <item.icon
+                      className={clsx(
+                        "mr-3 flex-shrink-0 h-6 w-6",
+                        item.current
+                          ? sidebar.activeIconColor
+                          : sidebar.inactiveIconColor
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                </Link>
+              ))}
+            </nav>
+          </div>
         </div>
       </div>
-    </>
+
+      {/* Top bar + Content container */}
+      <div className="md:pl-64 flex flex-col flex-1">
+
+        {/* Top bar container */}
+        <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
+
+          {/* Mobile Hamburger button */}
+          <button
+            type="button"
+            className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 md:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+
+          {/* Top bar */}
+          <div className="flex-1 px-4 flex justify-between">
+            {/* Search input */}
+            <div className="flex-1 flex">
+              <form className="w-full flex md:ml-0" action="#" method="GET">
+                <label htmlFor="search-field" className="sr-only">
+                  Search
+                </label>
+                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                  <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                    <SearchIcon className="h-5 w-5" aria-hidden="true" />
+                  </div>
+                  <input
+                    id="search-field"
+                    className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                    placeholder="Search"
+                    type="search"
+                    name="search"
+                  />
+                </div>
+              </form>
+            </div>
+
+            {/* Utility Buttons */}
+            <div className="ml-4 flex items-center md:ml-6">
+              <button
+                type="button"
+                className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+              >
+                <span className="sr-only">View notifications</span>
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+
+              <AvatarDropdown />
+            </div>
+          </div>
+        </div>
+
+        {/* Content area */}
+        <main className="z-0 h-screen-nav overflow-y-scroll">
+          <div className="pt-6 pb-20">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              {children}
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
 
