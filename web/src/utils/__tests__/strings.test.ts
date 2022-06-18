4ccd56932bd5ac5b7d30e9b4e64e3a050434d14e
scottkuecker <kueckerscott@gmail.com>
2022-06-17 21:32:21 -0400

    web: implemented formatGrade() with unit tests

--- web/src/utils/__tests__/strings.test.ts
+++ web/src/utils/__tests__/strings.test.ts
@@ -1,4 +1,4 @@
-import { formatOrdinal } from "@utils/strings";
+import { formatGrade, formatOrdinal } from "@utils/strings";
 
 describe("strings", () => {
   describe("formatOrdinals()", () => {
@@ -21,6 +21,49 @@ describe("strings", () => {
         expect(formatOrdinal(-3)).toBe("-3rd");
         expect(formatOrdinal(-4)).toBe("-4th");
         expect(formatOrdinal(-10)).toBe("-10th");
+      });
+    });
+  });
+
+  describe("formatGrade()", () => {
+    describe("happy path", () => {
+      test("should return correct output for various number grades", () => {
+        expect(formatGrade("1", true)).toBe("1st");
+        expect(formatGrade("2", true)).toBe("2nd");
+        expect(formatGrade("3", true)).toBe("3rd");
+        expect(formatGrade("4", true)).toBe("4th");
+        expect(formatGrade("10", true)).toBe("10th");
+        expect(formatGrade("    10    ", true)).toBe("10th");
+
+        expect(formatGrade("1", false)).toBe("1st");
+        expect(formatGrade("2", false)).toBe("2nd");
+        expect(formatGrade("3", false)).toBe("3rd");
+        expect(formatGrade("4", false)).toBe("4th");
+        expect(formatGrade("10", false)).toBe("10th");
+        expect(formatGrade("    10    ", false)).toBe("10th");
+      });
+      test("should return correct output for 'K'", () => {
+        expect(formatGrade("k", false)).toBe("k");
+        expect(formatGrade("K", false)).toBe("K");
+        expect(formatGrade("    k    ", false)).toBe("k");
+        expect(formatGrade("    K    ", false)).toBe("K");
+
+        expect(formatGrade("k", true)).toBe("kindergarten");
+        expect(formatGrade("K", true)).toBe("Kindergarten");
+        expect(formatGrade("    k    ", true)).toBe("kindergarten");
+        expect(formatGrade("    K    ", true)).toBe("Kindergarten");
+      });
+    });
+    describe("sad path", () => {
+      test("should handle bad/weird input gracefully", () => {
+        expect(formatGrade("")).toBe("");
+        expect(formatGrade(" ")).toBe(" ");
+        expect(formatGrade("fooooo")).toBe("fooooo");
+        expect(formatGrade("kk")).toBe("kk");
+        expect(formatGrade("kindergarten")).toBe("kindergarten");
+
+        expect(formatGrade("-10")).toBe("-10th");
+        expect(formatGrade("0")).toBe("0th");
       });
     });
   });
--- web/src/utils/strings.ts
+++ web/src/utils/strings.ts
@@ -32,3 +32,39 @@ export function formatOrdinal(count: number) {
   const suffix = suffixes[locale][rule];
   return `${count}${suffix}`;
 }
+
+/**
+ * Takes a grade string (typically 1-12 or K) and returns a nicely formatted
+ * ordinal string for display purposes.
+ * "1" -> "1st", "3" -> "3rd", "k"/"K" -> "k"/"K" or "k/Kindergarten"
+ *
+ * Maintaining capitalization of the word "Kindergarten" is a handy convenience,
+ * just pass whatever grade with .toLowerCase() or .toUpperCase() to get the
+ * desired result.
+ *
+ * @param grade string grade value, typically a number or "k"
+ * @param longK will convert a "k" into "kindergarten"; "K" to "Kindergarten"
+ * @returns
+ */
+export function formatGrade(grade: string, longK = false) {
+  const trimmedGrade = grade.trim();
+
+  // If an empty string return unchanged.
+  if (!trimmedGrade) {
+    return grade;
+  }
+
+  // If a "k" or "K".
+  if (trimmedGrade.toLowerCase() === "k") {
+    return longK ? trimmedGrade + "indergarten" : trimmedGrade;
+  }
+
+  // Parse a number.
+  const gradeNumber = Number(trimmedGrade);
+  if (isNaN(gradeNumber)) {
+    return trimmedGrade;
+  }
+
+  // Return the ordinal.
+  return formatOrdinal(gradeNumber);
+}
