03e3dcc8d6d3d3e1cc67859b6a66d9eca479d7ec
anthonypetter <anthonypsalazar9@gmail.com>
2022-06-26 18:42:06 -0600

    web: simplify page header component and allow for broader range of children

--- web/src/components/PageHeader.tsx
+++ web/src/components/PageHeader.tsx
@@ -1,38 +1,42 @@
 import clsx from "clsx";
+import { ReactNode } from "react";
 import { Breadcrumbs, type Breadcrumb } from "./Breadcrumbs";
 
 type Props = {
   title: string;
-  description?: string | null;
   breadcrumbs?: Breadcrumb[];
+  children?: ReactNode;
 };
 
-export function PageHeader({ title, description, breadcrumbs }: Props) {
+export function PageHeader({ title, breadcrumbs, children }: Props) {
   return (
     <>
       {breadcrumbs && <Breadcrumbs path={breadcrumbs} />}
-      <div
-        className={clsx(
-          "flex flex-col sm:flex-row sm:items-center sm:justify-between",
-          "mb-8"
-        )}
-      >
-        <div className="flex items-center">
-          <div className="flex">
-            <div>
-              <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mb-2">
-                {title}
-              </h1>
-
-              {description && (
-                <p className="text-sm font-medium text-gray-500">
-                  {description}
-                </p>
-              )}
-            </div>
-          </div>
+      <div className={clsx("flex flex-col")}>
+        <div>
+          <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mb-2">
+            {title}
+          </h1>
         </div>
+
+        <div>{children}</div>
       </div>
     </>
   );
 }
+
+function DescriptionText({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={clsx("text-sm font-medium text-gray-500", className)}>
+      {children}
+    </div>
+  );
+}
+
+PageHeader.DescriptionText = DescriptionText;
