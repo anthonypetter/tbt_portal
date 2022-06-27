34e32b169edb453a63da4f46413288a49aadab93
scottkuecker <kueckerscott@gmail.com>
2022-06-27 08:32:50 -0700

    grahql: remove legacy files and add to .env

--- graphql/src/config/index.ts
+++ graphql/src/config/index.ts
@@ -20,7 +20,22 @@ export const WHEREBY_API_KEY = fromJust(
   "process.env.WHEREBY_API_KEY"
 );
 
-export const S3_BUCKET = fromJust(
-  process.env.S3_BUCKET,
-  "process.env.S3_BUCKET"
+export const S3_RECORDINGS_BUCKET = fromJust(
+  process.env.S3_RECORDINGS_BUCKET,
+  "process.env.S3_BUCKET_NAME"
+);
+
+export const AWS_ACCESS_ID = fromJust(
+  process.env.AWS_ACCESS_ID,
+  "process.env.AWS_ACCESS_ID"
+);
+
+export const AWS_SECRET_ACCESS_KEY = fromJust(
+  process.env.AWS_SECRET_ACCESS_KEY,
+  "process.env.AWS_SECRET_ACCESS_KEY"
+);
+
+export const AWS_REGION = fromJust(
+  process.env.AWS_REGION,
+  "process.env.AWS_REGION"
 );
