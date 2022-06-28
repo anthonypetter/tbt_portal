2d036f3a3f066c27d2f045bb6b26b9220cd8b964
anthonypetter <anthonypsalazar9@gmail.com>
2022-06-27 20:47:16 -0600

    web: update csv validation tests

--- web/src/utils/csv/__tests__/validateCsv.test.ts
+++ web/src/utils/csv/__tests__/validateCsv.test.ts
@@ -1,81 +1,177 @@
-import { validateCsv, validateCsvFile } from "../validateCsv";
+import { normalizeToUtcDate } from "@utils/dateTime";
+import { CsvValidationErrorMessage } from "@utils/errors";
 import fs from "fs";
 import path from "path";
-import { CsvValidationErrorMessage } from "@utils/errors";
+import { validate, validateCsv } from "../validateCsv";
 
 describe("csvValidation", () => {
   test("should detect invalid format for null", () => {
-    const { errors } = validateCsv(null);
+    const startDate = normalizeToUtcDate(new Date("8/1/2022"));
+    const endDate = normalizeToUtcDate(new Date("9/1/2022"));
+
+    const { errors } = validateCsv({
+      unvalidatedCsv: null,
+      startDate,
+      endDate,
+    });
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for undefined", () => {
-    const { errors } = validateCsv(undefined);
+    const startDate = normalizeToUtcDate(new Date("8/1/2022"));
+    const endDate = normalizeToUtcDate(new Date("9/1/2022"));
+
+    const { errors } = validateCsv({
+      unvalidatedCsv: undefined,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for empty object", () => {
-    const { errors } = validateCsv({});
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"));
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"));
+
+    const { errors } = validateCsv({
+      unvalidatedCsv: {},
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for random string", () => {
-    const { errors } = validateCsv("random string");
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"));
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"));
+
+    const { errors } = validateCsv({
+      unvalidatedCsv: "random string",
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect invalid format for random number", () => {
-    const { errors } = validateCsv(1234);
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"));
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"));
+
+    const { errors } = validateCsv({
+      unvalidatedCsv: 1234,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.invalidFormat);
   });
 
   test("should detect empty array", () => {
-    const { errors } = validateCsv([]);
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"));
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"));
+
+    const { errors } = validateCsv({
+      unvalidatedCsv: [],
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.emptyData);
   });
 
   test("should detect empty CSV", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-empty.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.emptyData);
   });
 
   test("should detect missing header", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-missing-header.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.rowLengthMismatch);
   });
 
   test("should detect row column mismatch", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-row-column-mismatch.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     expect(errors[0].message).toBe(CsvValidationErrorMessage.rowLengthMismatch);
   });
 
   test("should detect a missing column due to typo", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-header-typo.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     const error = errors[0];
     expect(error.message).toBe(CsvValidationErrorMessage.missingRequiredColumn);
@@ -83,11 +179,23 @@ describe("csvValidation", () => {
   });
 
   test("should detect multiple missing columns", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-missing-required-columns.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     const error = errors[0];
     expect(error.message).toBe(CsvValidationErrorMessage.missingRequiredColumn);
@@ -95,20 +203,44 @@ describe("csvValidation", () => {
   });
 
   test("should pass validation when optional columns are missing", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./valid-missing-optional-columns.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toEqual(0);
   });
 
   test("should fail due to mismatched column data", async () => {
+    const startDate = normalizeToUtcDate(new Date("1/1/2022"))
+      .getTime()
+      .toString();
+    const endDate = normalizeToUtcDate(new Date("1/1/2023"))
+      .getTime()
+      .toString();
+
     const readstream = fs.createReadStream(
       path.resolve(__dirname, "./invalid-mismatched-column-data.csv")
     );
 
-    const { errors } = await validateCsvFile(readstream);
+    const { errors } = await validate({
+      data: readstream,
+      startDate,
+      endDate,
+    });
+
     expect(errors.length).toBeGreaterThan(0);
     const error = errors[0];
     expect(error.message).toBe(CsvValidationErrorMessage.missingTeacherEmail);
