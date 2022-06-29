c12127afea24fe48d1d55bf61788dfc6d41793ba
scottkuecker <kueckerscott@gmail.com>
2022-06-29 16:23:13 -0300

    web: fixes en base of comments in PR

--- web/src/components/ContextMenu.tsx
+++ web/src/components/ContextMenu.tsx
@@ -2,59 +2,14 @@ import { DotsMenu } from "./DotsMenu";
 import { Menu } from "@headlessui/react";
 import { MailIcon, PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
 import clsx from "clsx";
-import { UserTableData } from "./users/UsersTable";
-import { ApolloError, gql, useMutation } from "@apollo/client";
-import { InviteUserMutation } from "@generated/graphql";
-import { fromJust } from "@utils/types";
-import { UserRole } from "@generated/graphql";
-import toast from "react-hot-toast";
-
-const INVITE_USER = gql`
-  mutation InviteUser($input: InviteUserInput!) {
-    inviteUser(input: $input) {
-      id
-    }
-  }
-`;
 
 type Props = {
-  userData: UserTableData
   onClickEdit?: () => void;
   onClickDelete?: () => void;
+  onClickInviteUser?: () => void;
 };
 
-export function ContextMenu({ userData, onClickEdit, onClickDelete }: Props) {
-  const [inviteUser] = useMutation<InviteUserMutation>(
-    INVITE_USER,
-    {
-      onError: (error: ApolloError) => { toast.error(error.message) },
-      onCompleted: () => toast.success("Invite sent")
-    }
-  )
-
-  const handleOnSendInvite = async (userData: UserTableData) => {
-    switch (userData.role) {
-      case "Admin": userData.role = UserRole.Admin
-        break;
-      case "Mentor Teacher": userData.role = UserRole.MentorTeacher
-        break;
-      case "Tutor Teacher": userData.role = UserRole.TutorTeacher
-        break;
-      default:
-        break;
-    }
-
-    await inviteUser({
-      variables: {
-        input: {
-          email: fromJust(userData.email, "email"),
-          fullName: fromJust(userData.fullName, "fullName"),
-          role: fromJust(userData.role, "role"),
-        },
-      },
-    });
-  }
-
+export function ContextMenu({ onClickInviteUser, onClickEdit, onClickDelete }: Props) {
   return (
     <DotsMenu>
       <Menu.Items className="absolute z-10 right-0 mt-2 w-56 bg-white rounded-md focus:outline-none shadow-lg origin-top-right ring-1 ring-black ring-opacity-5">
@@ -123,7 +78,7 @@ export function ContextMenu({ userData, onClickEdit, onClickDelete }: Props) {
           </Menu.Item>
           <Menu.Item>
             {({ active }) => {
-              const isDisabled = userData.accountStatus.toLowerCase() !== "pending";
+              const isDisabled = onClickInviteUser == null;
               return (
                 <button
                   disabled={isDisabled}
@@ -135,7 +90,7 @@ export function ContextMenu({ userData, onClickEdit, onClickDelete }: Props) {
                         : "text-red-600",
                     "group flex items-center px-4 py-2 w-full text-sm"
                   )}
-                  onClick={() => handleOnSendInvite(userData)}
+                  {...(onClickInviteUser ? { onClick: onClickInviteUser } : {})}
                 >
                   <MailIcon
                     className={clsx(
