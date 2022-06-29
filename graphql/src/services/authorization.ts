d3638f25073dd147c99de3b557f7223b45bc7c7b
scottkuecker <kueckerscott@gmail.com>
2022-06-29 07:02:15 -0700

    graphql: added auth check if auser is staff(admin mentor or turor teacher)

--- graphql/src/services/authorization.ts
+++ graphql/src/services/authorization.ts
@@ -5,9 +5,30 @@ export const AuthorizationService = {
     return user.role === UserRole.ADMIN;
   },
 
+  isTutorTeacher(user: User): boolean {
+    return user.role === UserRole.TUTOR_TEACHER;
+  },
+
+  isMentorTeacher(user: User): boolean {
+    return user.role === UserRole.MENTOR_TEACHER;
+  },
+
   assertIsAdmin(user: User) {
     if (!AuthorizationService.isAdmin(user)) {
       throw new Error("User is not authorized to perform this function.");
+    }
+    return true;
+  },
+
+  assertIsStaff(user: User): boolean {
+    if (
+      !(
+        AuthorizationService.isAdmin(user) ||
+        AuthorizationService.isTutorTeacher(user) ||
+        AuthorizationService.isMentorTeacher(user)
+      )
+    ) {
+      throw new Error("User is not authorized to perform this actioin ");
     }
     return true;
   },
