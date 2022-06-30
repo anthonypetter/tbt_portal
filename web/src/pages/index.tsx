dd5e11bc034170609a87be8b760c6d98e8f887cb
anthonypetter <anthonypsalazar9@gmail.com>
2022-06-29 19:55:45 -0600

    web: set up home page rendering logic based on account role

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,22 @@
-import type { NextPage, GetServerSidePropsContext } from "next";
-import { getServerSideAuth } from "@utils/auth/server-side-auth";
+import { ApolloQueryResult, gql } from "@apollo/client";
+import {
+  HomePageAdminQuery,
+  HomePageMentorQuery,
+  HomePageTutorQuery,
+  UserRole,
+} from "@generated/graphql";
+import { getSession } from "@lib/apollo-client";
+import { processResult } from "@utils/apollo";
+import {
+  fetchCurrentUser,
+  getServerSideAuth,
+} from "@utils/auth/server-side-auth";
+import { NotFoundError } from "@utils/errors";
+import { assertUnreachable, fromJust } from "@utils/types";
 import { AuthedLayout } from "components/AuthedLayout";
-import { PageHeader } from "components/PageHeader";
+import { MentorTeacherHome } from "components/home/MentorTeacherHome";
+import { TutorTeacherHome } from "components/home/TutorTeacherHome";
+import type { GetServerSidePropsContext, NextPage } from "next";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const auth = await getServerSideAuth(context);
@@ -10,19 +25,155 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     return { redirect: auth.redirect };
   }
 
+  const currentUser = await fetchCurrentUser(auth.token);
+
+  if (!currentUser) {
+    throw new NotFoundError();
+  }
+
+  const homeData = await fetchAccordingToRole(currentUser.role, auth.token);
+
   return {
     props: {
-      hello: "world",
+      homeData,
     },
   };
 }
 
-const Home: NextPage = () => {
-  return (
-    <AuthedLayout>
-      <PageHeader title="Home" />
-    </AuthedLayout>
-  );
+type Props = {
+  homeData: HomeResults;
+};
+
+const Home: NextPage<Props> = ({ homeData }) => {
+  switch (homeData.role) {
+    case UserRole.Admin:
+      return <AuthedLayout>Admin</AuthedLayout>;
+
+    case UserRole.MentorTeacher:
+      return (
+        <AuthedLayout>
+          <MentorTeacherHome
+            currentUser={fromJust(homeData.data.currentUser, "currentUser")}
+            teacherEngagements={homeData.data.teacherEngagements}
+          />
+        </AuthedLayout>
+      );
+
+    case UserRole.TutorTeacher:
+      return (
+        <AuthedLayout>
+          <TutorTeacherHome
+            currentUser={fromJust(homeData.data.currentUser, "currentUser")}
+          />
+        </AuthedLayout>
+      );
+
+    default:
+      assertUnreachable(homeData);
+  }
 };
 
 export default Home;
+
+type HomeResults =
+  | {
+      role: UserRole.Admin;
+      data: HomePageAdminQuery;
+    }
+  | {
+      role: UserRole.MentorTeacher;
+      data: HomePageMentorQuery;
+    }
+  | {
+      role: UserRole.TutorTeacher;
+      data: HomePageTutorQuery;
+    };
+
+async function fetchAccordingToRole(
+  role: UserRole,
+  token: string
+): Promise<HomeResults> {
+  switch (role) {
+    case UserRole.Admin: {
+      const data = await fetchAdminData(token);
+      return { role, data };
+    }
+
+    case UserRole.MentorTeacher: {
+      const data = await fetchMentorData(token);
+      return { role, data };
+    }
+
+    case UserRole.TutorTeacher: {
+      const data = await fetchTutorData(token);
+      return { role, data };
+    }
+
+    default:
+      assertUnreachable(role);
+  }
+}
+
+/**
+ * Admin Home  fetch
+ */
+
+const GET_HOME_DATA_ADMIN = gql`
+  query HomePageAdmin {
+    currentUser {
+      fullName
+    }
+  }
+`;
+
+async function fetchAdminData(token: string) {
+  const { client } = getSession(token);
+
+  const result: ApolloQueryResult<HomePageAdminQuery> = await client.query({
+    query: GET_HOME_DATA_ADMIN,
+  });
+
+  return processResult(result, (r) => r);
+}
+
+/**
+ * Mentor Tutor fetch
+ */
+
+const GET_HOME_DATA_MENTOR = gql`
+  query HomePageMentor {
+    ...MentorTeacherHome_Home
+  }
+  ${MentorTeacherHome.fragments.mentorHome}
+`;
+
+async function fetchMentorData(token: string) {
+  const { client } = getSession(token);
+
+  const result: ApolloQueryResult<HomePageMentorQuery> = await client.query({
+    query: GET_HOME_DATA_MENTOR,
+  });
+
+  return processResult(result, (r) => r);
+}
+
+/**
+ * Tutor Teacher fetch
+ */
+
+const GET_HOME_DATA_TUTOR = gql`
+  query HomePageTutor {
+    ...TutorTeacherHome_Home
+  }
+  ${TutorTeacherHome.fragments.tutorHome}
+`;
+
+async function fetchTutorData(token: string) {
+  const { client } = getSession(token);
+
+  const result: ApolloQueryResult<HomePageTutorQuery> = await client.query({
+    query: GET_HOME_DATA_TUTOR,
+  });
+
+  return processResult(result, (r) => r);
+}
