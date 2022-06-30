9d92be448cfbae7da9b7fffc9279480333b804a1
anthonypetter <anthonypsalazar9@gmail.com>
2022-06-29 19:58:36 -0600

    web: allow custom styles for assignment subject badge

--- web/src/components/AssignmentSubjectBadge.tsx
+++ web/src/components/AssignmentSubjectBadge.tsx
@@ -1,22 +1,38 @@
 import { AssignmentSubject } from "@generated/graphql";
 import { assertUnreachable } from "@utils/types";
+import clsx from "clsx";
 import React from "react";
 import { Badge } from "./Badge";
 
 type Props = {
   subject: AssignmentSubject;
+  className?: string;
 };
 
-export function AssignmentSubjectBadge({ subject }: Props) {
+export function AssignmentSubjectBadge({ subject, className }: Props) {
   switch (subject) {
     case AssignmentSubject.Math:
-      return <Badge className="text-purple-800 bg-purple-100 mr-5">Math</Badge>;
+      return (
+        <Badge
+          className={clsx("text-purple-800 bg-purple-100 mr-5", className)}
+        >
+          Math
+        </Badge>
+      );
 
     case AssignmentSubject.Ela:
-      return <Badge className="text-green-800 bg-green-100 mr-5">ELA</Badge>;
+      return (
+        <Badge className={clsx("text-green-800 bg-green-100 mr-5", className)}>
+          ELA
+        </Badge>
+      );
 
     case AssignmentSubject.General:
-      return <Badge className="text-blue-800 bg-blue-100 mr-5">General</Badge>;
+      return (
+        <Badge className={clsx("text-blue-800 bg-blue-100 mr-5", className)}>
+          General
+        </Badge>
+      );
 
     default:
       assertUnreachable(subject);
