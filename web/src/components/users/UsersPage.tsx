0a1c1404fc5dd3614cda97669a49ed1d427ba334
anthonypetter <anthonypsalazar9@gmail.com>
2022-06-30 18:12:14 -0600

    web: ensure table is updated after invite is sent

--- web/src/components/users/UsersPage.tsx
+++ web/src/components/users/UsersPage.tsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
-import { Button } from "../Button";
-import { InviteUserModal } from "./InviteUserModal";
-import { SearchUsersForUsersPageQuery } from "@generated/graphql";
 import { gql, useLazyQuery } from "@apollo/client";
-import { triggerSuccessToast, triggerErrorToast } from "../Toast";
-import { UsersTable } from "./UsersTable";
+import { SearchUsersForUsersPageQuery } from "@generated/graphql";
+import { HomeIcon, SearchIcon } from "@heroicons/react/solid";
 import { Routes } from "@utils/routes";
-import { PageHeader } from "components/PageHeader";
-import { HomeIcon } from "@heroicons/react/solid";
 import { Input } from "components/Input";
-import { SearchIcon } from "@heroicons/react/solid";
+import { PageHeader } from "components/PageHeader";
 import { Spinner } from "components/Spinner";
+import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
+import { Button } from "../Button";
+import { triggerErrorToast, triggerSuccessToast } from "../Toast";
+import { InviteUserModal } from "./InviteUserModal";
+import { UsersTable } from "./UsersTable";
 
 const MIN_QUERY_LENGTH = 3;
 
 type Props = {
-  users: SearchUsersForUsersPageQuery['searchUsers']['results'];
+  users: SearchUsersForUsersPageQuery["searchUsers"]["results"];
   refetchUsers: () => void;
 };
 
@@ -30,25 +29,28 @@ UsersPage.fragments = {
 };
 
 const SEARCH_USER = gql`
-query SearchUsersForUsersPage($query: String!) {
-  searchUsers(query: $query) {
-    count
-    results {
-      id
-      fullName
-      email
-      role
-      accountStatus
-      inviteSentAt
+  query SearchUsersForUsersPage($query: String!) {
+    searchUsers(query: $query) {
+      count
+      results {
+        id
+        fullName
+        email
+        role
+        accountStatus
+        inviteSentAt
+      }
     }
   }
-}`;
+`;
 
 export function UsersPage({ users, refetchUsers }: Props) {
   const [showInviteModal, setShowInviteModal] = useState(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [searchResultsMode, setSearchResultsMode] = useState(false);
-  const [searchResults, setSearchResults] = useState<SearchUsersForUsersPageQuery['searchUsers']['results']>([]);
+  const [searchResults, setSearchResults] = useState<
+    SearchUsersForUsersPageQuery["searchUsers"]["results"]
+  >([]);
 
   const { loading } = useUsersSearch(searchQuery, {
     onCompleted: (results) => {
@@ -76,17 +78,19 @@ export function UsersPage({ users, refetchUsers }: Props) {
       </div>
 
       <div className="flex justify-between mb-6">
-        <Input
-          className="flex-1 max-w-lg"
-          id="users-search"
-          placeholder="Search Users"
-          leftIcon={SearchIcon}
-          value={searchQuery}
-          type="search"
-          onChange={(event) => setSearchQuery(event.target.value)}
-        />
-        <div className="w-12 ml-1">
-          {loading && <Spinner color="border-blue-500" />}
+        <div className="flex flex-1 max-w-lg items-center">
+          <Input
+            className="flex-1"
+            id="users-search"
+            placeholder="Search Users"
+            leftIcon={SearchIcon}
+            value={searchQuery}
+            type="search"
+            onChange={(event) => setSearchQuery(event.target.value)}
+          />
+          <div className="w-12 ml-1">
+            {loading && <Spinner color="border-blue-500" />}
+          </div>
         </div>
         <button
           className="text-blue-500"
@@ -120,33 +124,35 @@ export function UsersPage({ users, refetchUsers }: Props) {
 const useUsersSearch = (
   query: string,
   options: {
-    onCompleted: (results: SearchUsersForUsersPageQuery['searchUsers']['results']) => void
+    onCompleted: (
+      results: SearchUsersForUsersPageQuery["searchUsers"]["results"]
+    ) => void;
   }
 ) => {
-  const [debouncedQuery] = useDebounce(query, 300)
+  const [debouncedQuery] = useDebounce(query, 300);
 
   const [searchUsers, { loading, data, error }] =
     useLazyQuery<SearchUsersForUsersPageQuery>(SEARCH_USER, {
       variables: { query },
       fetchPolicy: "no-cache",
       onCompleted: ({ searchUsers }) => {
-        options.onCompleted(searchUsers.results)
+        options.onCompleted(searchUsers.results);
       },
       onError: (error) => {
         triggerErrorToast({
           message: "Something went wrong with this search.",
           sub: error.message,
-        })
-      }
-    })
+        });
+      },
+    });
 
   useEffect(() => {
     if (debouncedQuery.length >= MIN_QUERY_LENGTH) {
       searchUsers({
-        variables: { query: debouncedQuery }
-      })
+        variables: { query: debouncedQuery },
+      });
     }
   }, [debouncedQuery, searchUsers]);
 
   return { loading, error, results: data?.searchUsers.results };
-}
+};
